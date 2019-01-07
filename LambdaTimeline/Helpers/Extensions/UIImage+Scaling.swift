39291b0d02b93b1618f17e49d78a28890a9529e3
OmegaWulf <nikcxz@gmail.com>
2019-01-07 18:47:09 -0500

    initial code done

---
+++ LambdaTimeline/Helpers/Extensions/UIImage+Scaling.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+Scaling.swift
+//  LambdaTimeline
+//
+//  Created by Nikita Thomas on 1/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import ImageIO
+
+extension UIImage {
+    func imageByScaling(toSize size: CGSize) -> UIImage? {
+        guard let data = pngData(),
+            let imageSource = CGImageSourceCreateWithData(data as CFData, nil) else {
+                return nil
+        }
+        
+        let options: [CFString: Any] = [
+            kCGImageSourceThumbnailMaxPixelSize: max(size.width, size.height) / 2.0,
+            kCGImageSourceCreateThumbnailFromImageAlways: true
+        ]
+        
+        return CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options as CFDictionary).flatMap { UIImage(cgImage: $0) }
+    }
+}
+
--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -11,6 +11,48 @@ import Photos
 
 class ImagePostViewController: ShiftableViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var chooseImageButton: UIButton!
+    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var postButton: UIBarButtonItem!
+    
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    
+    @IBOutlet weak var filterControls: UIView!
+    
+    var postController: PostController!
+    var post: Post?
+    var imageData: Data?
+    
+    
+    private let context = CIContext(options: nil)
+    private let filter = CIFilter(name: "CIColorControls")!
+    
+    var originalImage: UIImage? {
+        didSet {
+            
+            guard let originalImage = originalImage else { return }
+            
+            var scaledSize = imageView.bounds.size
+            
+            // 1x, 2x, or 3x
+            let scale = UIScreen.main.scale
+            
+            scaledSize = CGSize(width: scaledSize.width * scale, height: scaledSize.height * scale)
+            
+            scaledImage = originalImage.imageByScaling(toSize: scaledSize)
+        }
+    }
+    
+    var scaledImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,6 +78,58 @@ class ImagePostViewController: ShiftableViewController {
         chooseImageButton.setTitle("", for: [])
     }
     
+    
+    @IBAction func changeBrightness(_ sender: Any) {
+        updateImage()
+    }
+    
+    @IBAction func changeContrast(_ sender: Any) {
+        updateImage()
+    }
+    
+    @IBAction func changeSaturation(_ sender: Any) {
+        updateImage()
+    }
+    
+    
+    
+    // Filter functions
+    func updateImage() {
+        if let scaledImage  = scaledImage {
+            imageView.image = image(byFiltering: scaledImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    func image(byFiltering image: UIImage) -> UIImage {
+        
+        guard let cgImage = image.cgImage else { return image}
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        // Set the values of the filters parameters
+        filter.setValue(ciImage, forKey: "inputImage")
+        filter.setValue(saturationSlider.value, forKey: "inputSaturation")
+        filter.setValue(brightnessSlider.value, forKey: "inputBrightness")
+        filter.setValue(contrastSlider.value, forKey: "inputContrast")
+        
+        // The metadata to be processed. NOT the actual filtered image
+        guard let outputCIImage = filter.outputImage else { return image }
+        
+        // This is a visual Image that has been processed
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return image }
+        
+        return UIImage(cgImage: outputCGImage)
+        
+    }
+    
+    func showFilterControls() {
+        filterControls.isHidden = false
+    }
+    
+    
+    
     private func presentImagePickerController() {
         
         guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
@@ -48,18 +142,27 @@ class ImagePostViewController: ShiftableViewController {
         imagePicker.delegate = self
         
         imagePicker.sourceType = .photoLibrary
-
+        
         present(imagePicker, animated: true, completion: nil)
     }
+    
+    
+    
+    
+    
     
     @IBAction func createPost(_ sender: Any) {
         
         view.endEditing(true)
         
-        guard let imageData = imageView.image?.jpegData(compressionQuality: 0.1),
+        guard let originalImage = originalImage else { return }
+        
+        let processedImage = self.image(byFiltering: originalImage)
+        
+        guard let imageData = processedImage.jpegData(compressionQuality: 0.1),
             let title = titleTextField.text, title != "" else {
-            presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
-            return
+                presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
+                return
         }
         
         postController.createPost(with: title, ofType: .image, mediaData: imageData, ratio: imageView.image?.ratio) { (success) in
@@ -75,6 +178,10 @@ class ImagePostViewController: ShiftableViewController {
             }
         }
     }
+    
+    
+    
+    
     
     @IBAction func chooseImage(_ sender: Any) {
         
@@ -112,21 +219,13 @@ class ImagePostViewController: ShiftableViewController {
         view.layoutSubviews()
     }
     
-    var postController: PostController!
-    var post: Post?
-    var imageData: Data?
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var chooseImageButton: UIButton!
-    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var postButton: UIBarButtonItem!
 }
 
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
+        
         chooseImageButton.setTitle("", for: [])
         
         picker.dismiss(animated: true, completion: nil)
@@ -136,6 +235,9 @@ extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigation
         imageView.image = image
         
         setImageViewHeight(with: image.ratio)
+        
+        originalImage = image
+        showFilterControls()
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
