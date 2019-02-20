0e5dbf6cd9136b0e827d0002c9540c67e641620a
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-02-19 22:41:34 -0800

    Added AudioPostViewController

---
+++ LambdaTimeline/View Controllers/AudioPostViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AudioPostViewController.swift
+//  LambdaTimeline
+//
+//  Created by Ivan Caldwell on 2/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class AudioPostViewController: UIViewController {
+
+    // MARK: - Properties
+    var postController: PostController!
+    var post: Post?
+    var imageData: Data?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
