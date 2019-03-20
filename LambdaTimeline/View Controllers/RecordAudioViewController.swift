64d3590d29b5b5e6966ee12261867d6de2fdb2d7
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-20 01:03:23 -0700

    modal controller creates recodring and recording url. can cancel and clear recordings

--- LambdaTimeline/View Controllers/RecordAudioViewController.swift
+++ LambdaTimeline/View Controllers/RecordAudioViewController.swift
@@ -7,23 +7,125 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RecordAudioViewController: UIViewController {
+class RecordAudioViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        clearDirectoryOfRecordings()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    
+    @IBAction func record(_ sender: Any) {
+        
+        if isRecording {
+            recorder?.stop()
+            return
+        }
+        
+        do {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 2)!
+            
+            recorder = try AVAudioRecorder(url: newRecordingURL(), format: format)
+            recorder?.delegate = self
+            recorder?.record()
+        } catch {
+            NSLog("Unable to start recording")
+        }
+        
+        updateButtons()
     }
-    */
+    
+    @IBAction func play(_ sender: Any) {
+        
+        guard let recordingURL = recordingURL else { return }
+        
+        if isPLaying {
+            player?.stop()
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: recordingURL)
+            player?.play()
+            
+            player?.delegate = self
+        } catch {
+            NSLog("Error attempting to start playing audio: \(error)")
+        }
+        
+        updateButtons()
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+        clearDirectoryOfRecordings()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func postRecording(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Private Functions
+    
+    private func clearDirectoryOfRecordings() {
+        
+        let documentsDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        let documentURLs = try! FileManager.default.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        
+        var recordingURLs = documentURLs.filter({ $0.pathExtension == "caf" })
+        
+        recordingURLs.removeAll()
+    }
+    
+    private func updateButtons() {
+        
+        let playButtonTitle = isPLaying ? "Stop Playing" : "Play Back"
+        playButton.setTitle(playButtonTitle, for: .normal)
+        
+        let recordButtonTitle = isRecording ? "Stop Recording" : "Record Comment"
+        recordButton.setTitle(recordButtonTitle, for: .normal)
+    }
+    
+    private func newRecordingURL() -> URL {
+        
+        let documentsDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        return documentsDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+    }
 
+    // MARK: - AVAudio Delegates
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateButtons()
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        updateButtons()
+        
+        recordingURL = recorder.url
+    }
+    
+    // MARK: - Properties
+    
+    var recordingURL: URL?
+    
+    private var player: AVAudioPlayer?
+    
+    var isPLaying: Bool {
+        return player?.isPlaying ?? false
+    }
+    
+    private var recorder: AVAudioRecorder?
+    
+    var isRecording: Bool {
+        return recorder?.isRecording ?? false
+    }
+    
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var playButton: UIButton!
 }
