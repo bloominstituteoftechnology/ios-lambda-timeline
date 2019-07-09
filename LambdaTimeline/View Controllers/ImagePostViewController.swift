2e25f27fe6147cac0e4a124c6e4e1bd26bcf6b85
Michael R.F <mrflowers22@uwalumni.com>
2019-07-09 14:39:48 -0700

    commit and push so that I can branch and do day two

--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -11,6 +11,22 @@ import Photos
 
 class ImagePostViewController: ShiftableViewController {
     
+    //filter properties
+    private let controlFilter = CIFilter(name: "CIColorControls") //the name must match so refer to the documentation
+    private let blurFilter = CIFilter(name: "CIMotionBlur")
+    private let context = CIContext(options: nil)
+    var originalImage: UIImage? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    @IBOutlet weak var saturationSlider: UISlider!
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var radiusSlider: UISlider!
+    @IBOutlet weak var angleSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,22 +35,85 @@ class ImagePostViewController: ShiftableViewController {
         updateViews()
     }
     
+    @IBAction func saturationValuChanged(_ sender: UISlider) {
+        updateViews()
+    }
+    @IBAction func brightnessValueChanged(_ sender: UISlider) {
+        updateViews()
+    }
+    @IBAction func contrastValueChanged(_ sender: UISlider) {
+        updateViews()
+    }
+    @IBAction func raidiusValueChanged(_ sender: UISlider) {
+        updateViews()
+    }
+    @IBAction func angleValueChanged(_ sender: UISlider) {
+        updateViews()
+    }
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        //this is where we add the filters to the image
+        //we have to go from a CGImage to a CIImage
+        guard let cgImage = image.cgImage else { return image }
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        //set the filter properties
+        controlFilter?.setValue(ciImage, forKey: "inputImage") //this key must match so refer to documentation
+        
+        //can either type in the string name or the kCIInputKeys
+        controlFilter?.setValue(saturationSlider.value, forKey: "inputSaturation") //again we are getting the key from the documentation
+        controlFilter?.setValue(brightnessSlider.value, forKey: kCIInputBrightnessKey) //this is an example of where we use the key to avoid stringly typed code
+        controlFilter?.setValue(contrastSlider.value, forKey: kCIInputContrastKey)
+        
+        //motionBlurFilter
+        blurFilter?.setValue(controlFilter?.outputImage, forKey: "inputImage")
+        blurFilter?.setValue(radiusSlider.value, forKey: kCIInputRadiusKey)
+        blurFilter?.setValue(angleSlider.value, forKey: kCIInputAngleKey)
+        
+        guard let outputCIImage = blurFilter?.outputImage else { return image }
+        
+        //render the image - actually apply our filters
+        //go back from cIImage to cGIimage
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return image }
+        
+        //then we have to go from a cGImage to a UIImage
+        return UIImage(cgImage: outputCGImage)
+        
+    }
+   
+    
     func updateViews() {
         
-        guard let imageData = imageData,
-            let image = UIImage(data: imageData) else {
-                title = "New Post"
-                return
+        if let originalImage = originalImage {
+            imageView.image = image(byFiltering: originalImage)
+            title = post?.title
+            setImageViewHeight(with: originalImage.ratio)
+            chooseImageButton.setTitle("", for: [])
+        } else {
+            imageView.image = nil
+            title = "New Post"
         }
         
-        title = post?.title
         
-        setImageViewHeight(with: image.ratio)
+//        guard let imageData = imageData,
+//            let image = UIImage(data: imageData) else {
+//                return
+//        }
         
-        imageView.image = image
+//        imageView.image = image
+//        let myImage = image(byFiltering: image)
+//        imageView.image = image(byFiltering: image)
         
-        chooseImageButton.setTitle("", for: [])
     }
+    
+//    func updateImage(){
+//        guard let data = imageData else { print("Error right here too"); return }
+//        guard let image = UIImage(data: data) else { print("problem right here"); return }
+//        let myImage = image(byFiltering: image)
+//        if let currentImage = originalImage {
+//            imageView.image = image(byFiltering: currentImage)
+//        }
+//    }
     
     private func presentImagePickerController() {
         
@@ -114,7 +193,13 @@ class ImagePostViewController: ShiftableViewController {
     
     var postController: PostController!
     var post: Post?
-    var imageData: Data?
+    var imageData: Data? {
+        didSet {
+//            print("imageData set and triggered.")
+//            updateImage()
+        }
+    }
+  
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
@@ -125,20 +210,38 @@ class ImagePostViewController: ShiftableViewController {
 
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//        chooseImageButton.setTitle("", for: [])
+//
+//        picker.dismiss(animated: true, completion: nil)
+//
+//        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+//
+////        imageView.image = image
+//        originalImage = image
+//        setImageViewHeight(with: image.ratio)
+//    }
+//
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        picker.dismiss(animated: true, completion: nil)
+//    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
+        
         chooseImageButton.setTitle("", for: [])
         
+        if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        
         picker.dismiss(animated: true, completion: nil)
-        
-        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-        
-        imageView.image = image
-        
-        setImageViewHeight(with: image.ratio)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
 }
+
