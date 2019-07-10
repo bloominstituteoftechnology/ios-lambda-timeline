56a4fde1062a5c1c4cdad3b15eb1494df4638703
Michael R.F <mrflowers22@uwalumni.com>
2019-07-10 13:23:03 -0700

    test

--- LambdaTimeline/View Controllers/CameraViewController.swift
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -14,7 +14,7 @@ class CameraViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       //test
     }
     
 
