51f3d08a0fde74b4754807fca192d232dfa1c7de
Michael R.F <mrflowers22@uwalumni.com>
2019-07-10 13:53:23 -0700

    implemented best camera method

--- LambdaTimeline/View Controllers/CameraViewController.swift
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -16,10 +16,27 @@ class CameraViewController: UIViewController {
     private var player: AVPlayer!
     
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var cameraView: CameraPrviewView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //get the camera you want to use
+        
+        
+    }
+    
+    //set up the device you want to use on the camera
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back){
+                return device
+        }
+        
+        //could not get a camera
+        fatalError("No camera exists - you are running on an old ass phone or a simulator.")
     }
     
     @IBAction func recordButtonPressed(_ sender: UIButton) {
