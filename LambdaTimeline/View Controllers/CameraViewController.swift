bd24245194561f866c23f6cb880f4a476cb3045f
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-10 16:36:09 -0400

    Added video recording.  Currently saves to file not firebase.  Updated MediaType Enum to include video case.

--- LambdaTimeline/View Controllers/CameraViewController.swift
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -12,8 +12,140 @@ import AVFoundation
 class CameraViewController: UIViewController {
     
     
+    @IBOutlet weak var cameraView: CameraPreviewView!
+    @IBOutlet weak var recordButton: UIButton!
+
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    private var player: AVPlayer!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = bestCamera()
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else { fatalError("Can't create input from camera") }
+        
+        
+        // setup inputs
+        if captureSession.canAddInput(cameraInput) {
+            captureSession.addInput(cameraInput)
+        }
+        
+        guard let microphone = AVCaptureDevice.default(for: .audio) else {
+            fatalError("Can't find microphone")
+        }
+        
+        guard let microphoneInput = try? AVCaptureDeviceInput(device: microphone) else {
+            fatalError("Can't create input from microphone")
+        }
+        
+        if captureSession.canAddInput(microphoneInput) {
+            captureSession.addInput(microphoneInput)
+        }
+        
+        //setup outputs
+        
+        if captureSession.canAddOutput(fileOutput) {
+            captureSession.addOutput(fileOutput)
+        }
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.canSetSessionPreset(.hd1920x1080)
+        }
+        
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
     
     
     
+    @objc func handleTapGesture(_ tapGesture: UITapGestureRecognizer) {
+        //play movie
+        if let player = player {
+            player.seek(to: CMTime.zero)
+            player.play()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            return device
+        }
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        fatalError("No cameras exist")
+    }
+    
+    func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let name = "movie"
+        let url = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        print("URL: \(url)")
+        return url
+    }
+    
+    func updateViews() {
+        if fileOutput.isRecording {
+            recordButton.setImage(UIImage(named: "Stop"), for: .normal)
+            recordButton.tintColor = UIColor.black
+        } else {
+            recordButton.setImage(UIImage(named: "Record"), for: .normal)
+            recordButton.tintColor = UIColor.red
+        }
+    }
+    
+    func playMovie(url: URL) {
+        player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: player)
+        var topRect = self.view.bounds
+        topRect.size.width = topRect.width / 4
+        topRect.size.height = topRect.height / 4
+        topRect.origin.y = view.layoutMargins.top
+        playerLayer.frame = topRect
+        
+        view.layer.addSublayer(playerLayer)
+        
+        player.play()
+    }
+    
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
     
 }
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        DispatchQueue.main.async {
+            self.updateViews()
+            self.playMovie(url: outputFileURL)
+        }
+    }
+}
+    
+    
+
