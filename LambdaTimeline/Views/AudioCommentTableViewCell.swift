2c9cf0fd4098ee3e3c799030c8c79664634bc78c
jayfenam <jonathanferrer95@gmail.com>
2019-07-10 16:51:45 -0400

    audio comments are fetching and playing in tableview

--- LambdaTimeline/Views/AudioCommentTableViewCell.swift
+++ LambdaTimeline/Views/AudioCommentTableViewCell.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
+
+protocol AudioCommentTableViewCellDelegate: class {
+    func playRecording(for cell: AudioCommentTableViewCell)
+}
 
 class AudioCommentTableViewCell: UITableViewCell {
 
-    @IBAction func playButtonPressed(_ sender: Any) {
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        playRecordingButton.isEnabled = false
     }
 
+    @IBAction func playRecording(_ sender: Any) {
+        delegate?.playRecording(for: self)
+    }
+
+    func updateViews() {
+        guard let comment = comment else { return }
+        authorLabel.text = comment.author.displayName
+    }
+
+    weak var delegate: AudioCommentTableViewCellDelegate?
+
+    var audioData: Data! {
+        didSet {
+            playRecordingButton.isEnabled = audioData != nil
+        }
+    }
+    var comment: Comment! {
+        didSet {
+            updateViews()
+        }
+    }
 
     @IBOutlet weak var authorLabel: UILabel!
-    var comment: Comment?
+    @IBOutlet weak var playRecordingButton: UIButton!
 
 }
