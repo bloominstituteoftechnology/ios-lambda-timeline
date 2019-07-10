b6d39650f1af4c8b81ba31fe416c94e48015dbfa
Michael R.F <mrflowers22@uwalumni.com>
2019-07-09 18:03:01 -0700

    committing because the last one didn't include all of the files

--- LambdaTimeline/Models/Recorder.swift
+++ LambdaTimeline/Models/Recorder.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
-import Foundation
+import AVFoundation
+
+
+class Recorder: NSObject {
+    
+    private var audioRecorder: AVAudioRecorder?
+    
+    //what functionality do we want to abstract?
+    func toggleRecording(){
+        
+    }
+    
+    func record(){
+        
+    }
+    
+    func stop(){
+        audioRecorder?.stop() //we have to stop it and set it to nil to erase
+        audioRecorder = nil
+    }
+}
