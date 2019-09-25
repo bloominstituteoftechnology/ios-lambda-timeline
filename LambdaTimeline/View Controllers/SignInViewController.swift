00515e96a409a441862dd04e76cc05351e5c5810
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-09-25 00:23:34 -0400

    updated the assets and the App Delegate

--- LambdaTimeline/View Controllers/SignInViewController.swift
+++ LambdaTimeline/View Controllers/SignInViewController.swift
@@ -44,8 +44,8 @@ class SignInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDele
             
             DispatchQueue.main.async {
                 let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                let postsNavigationController = storyboard.instantiateViewController(withIdentifier: "PostsNavigationController")
-                self.present(postsNavigationController, animated: true, completion: nil)
+                let postsTabBarController = storyboard.instantiateViewController(withIdentifier: "PostsTabBarController")
+                self.present(postsTabBarController, animated: true, completion: nil)
             }
         }
     }
