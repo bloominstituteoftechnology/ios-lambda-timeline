a65c0c606d714fae3a65a75ca4363192bc6ef3e1
bradleyyin <bradleyyin@gmail.com>
2019-10-01 15:53:21 -0700

    add audio comment vc

---
+++ LambdaTimeline/Helpers/Recorder.swift
@@ -0,0 +1,9 @@
+//
+//  Recorder.swift
+//  LambdaTimeline
+//
+//  Created by Bradley Yin on 10/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ LambdaTimeline/View Controllers/AudioCommentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AudioCommentViewController.swift
+//  LambdaTimeline
+//
+//  Created by Bradley Yin on 10/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AudioCommentViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recordButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func sendButtonTapped(_ sender: UIButton) {
+    }
+    
+}
