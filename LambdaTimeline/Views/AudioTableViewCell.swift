6f430024b91b303662317432605e663a44fd2c1e
luqmaan <luq.j.khan@gmail.com>
2019-10-01 15:57:00 -0500

    add audio cell and sublassed view

---
+++ LambdaTimeline/Views/AudioTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AudioTableViewCell.swift
+//  LambdaTimeline
+//
+//  Created by Luqmaan Khan on 10/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AudioTableViewCell: UITableViewCell {
+
+    @IBOutlet var sendersName: UILabel!
+    
+    @IBOutlet var playRecording: UIButton!
+    
+    @IBOutlet var playRecordingTapped: UIButton!
+}
