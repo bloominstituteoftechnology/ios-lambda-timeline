f87f8ddedcf265b90eb155e8c5525f528cdddaab
Michael Redig <mredig@gmail.com>
2019-10-02 16:21:39 -0500

    started designing recording ui

---
+++ LambdaTimeline/View Controllers/Post Creation/VideoRecordingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  VideoRecordingViewController.swift
+//  LambdaTimeline
+//
+//  Created by Michael Redig on 10/2/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class VideoRecordingViewController: UIViewController {
+	/// hide this when video is recording - makes it really hard to accidentally NOT record
+	@IBOutlet private var indicatorContainer: UIView!
+	@IBOutlet private var recordButton: UIButton!
+	@IBOutlet private var playbackButton: UIButton!
+
+	
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		recordButton.tintColor = .systemRed
+		playbackButton.tintColor = .systemGreen
+	}
+
+}
