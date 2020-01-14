cfddf8b560f40fc3717a192f4d241d2734df8edd
Jon Bash <jonbash@protonmail.com>
2020-01-14 14:01:26 -0800

    add properties for playbackControl

--- LambdaTimeline/Views/AudioPlaybackControl.swift
+++ LambdaTimeline/Views/AudioPlaybackControl.swift
@@ -11,3 +11,30 @@ import AVFoundation
 
 @IBDesignable
 class AudioPlaybackControl: UIControl {
+
+    // MARK: - UI Elements
+
+    private(set) var playPauseButton = UIButton()
+    private(set) var timeSlider = UISlider()
+
+    private var timestampLabel = UILabel()
+    private var timeRemainingLabel = UILabel()
+
+    private var verticalStack: UIStackView!
+    private var sliderStack: UIStackView!
+
+    private var timeFormatter: DateComponentsFormatter {
+        DateComponentsFormatter.audioPlayerTimeFormatter
+    }
+
+    // MARK: - Audio Properties
+
+    private(set) var audioFileURL: URL?
+
+    var isPlaying: Bool { audioPlayer?.isPlaying ?? false }
+    var elapsedTime: TimeInterval { audioPlayer?.currentTime ?? 0 }
+    var totalDuration: TimeInterval { audioPlayer?.duration ?? 0 }
+    var timeRemaining: TimeInterval { totalDuration - elapsedTime }
+
+    private var audioPlayer: AVAudioPlayer?
+    private var uiUpdateTimer: Timer?
