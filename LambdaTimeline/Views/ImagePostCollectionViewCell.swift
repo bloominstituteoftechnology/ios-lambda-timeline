3a0abe61d0fc300859038b0bf74448cfaa15980e
Jon Bash <jonbash@protonmail.com>
2020-01-14 14:00:30 -0800

    pare down imagePostCVCell

--- LambdaTimeline/Views/ImagePostCollectionViewCell.swift
+++ LambdaTimeline/Views/ImagePostCollectionViewCell.swift
@@ -8,43 +8,17 @@
 
 import UIKit
 
-class ImagePostCollectionViewCell: UICollectionViewCell {
-
-    var post: Post? {
-        didSet {
-            updateViews()
-        }
-    }
+class ImagePostCollectionViewCell: PostCollectionViewCell {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var labelBackgroundView: UIView!
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        setupLabelBackgroundView()
-    }
+
     override func prepareForReuse() {
         super.prepareForReuse()
-        
         imageView.image = nil
-        titleLabel.text = ""
-        authorLabel.text = ""
-    }
-    
-    func updateViews() {
-        guard let post = post else { return }
-    
-        titleLabel.text = post.title
-        authorLabel.text = post.author.displayName
     }
 
-    func setupLabelBackgroundView() {
-        labelBackgroundView.layer.cornerRadius = 8
-//        labelBackgroundView.layer.borderColor = UIColor.white.cgColor
-//        labelBackgroundView.layer.borderWidth = 0.5
-        labelBackgroundView.clipsToBounds = true
+    override func updateViews() {
+        super.updateViews()
     }
     
     func setImage(_ image: UIImage?) {
