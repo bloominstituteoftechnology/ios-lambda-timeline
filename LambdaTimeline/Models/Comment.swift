aebbd637bc6e1c8428da91403465c75097f473d3
Jon Bash <jonbash@protonmail.com>
2020-01-14 15:35:45 -0800

    add audio comment

--- LambdaTimeline/Models/Comment.swift
+++ LambdaTimeline/Models/Comment.swift
@@ -9,31 +9,47 @@
 import Foundation
 import FirebaseAuth
 
-class Comment: FirebaseConvertible, Equatable {
+enum CommentType {
+    case text
+    case audio
+}
 
+class Comment: FirebaseConvertible, Equatable {
     static private let textKey = "text"
+    static private let audioKey = "audioURL"
     static private let author = "author"
     static private let timestampKey = "timestamp"
 
-    let text: String
+    let text: String?
+    let audioURL: URL?
     let author: Author
     let timestamp: Date
     
     init(text: String, author: Author, timestamp: Date = Date()) {
+        self.audioURL = nil
         self.text = text
+        self.author = author
+        self.timestamp = timestamp
+    }
+
+    init(audioURL: URL, author: Author, timestamp: Date = Date()) {
+        self.audioURL = audioURL
+        self.text = nil
         self.author = author
         self.timestamp = timestamp
     }
     
     init?(dictionary: [String : Any]) {
         guard
-            let text = dictionary[Comment.textKey] as? String,
             let authorDictionary = dictionary[Comment.author] as? [String: Any],
             let author = Author(dictionary: authorDictionary),
             let timestampTimeInterval = dictionary[Comment.timestampKey] as? TimeInterval
             else { return nil }
+        let text = dictionary[Comment.textKey] as? String
+        let audioURL = dictionary[Comment.audioKey] as? URL
         
         self.text = text
+        self.audioURL = audioURL
         self.author = author
         self.timestamp = Date(timeIntervalSince1970: timestampTimeInterval)
     }
