55d2530b3c5199c8e7a1eab96d642c9c910cbb2a
Jon Bash <jonbash@protonmail.com>
2020-01-14 14:06:17 -0800

    start audiopostVC

---
+++ LambdaTimeline/View Controllers/AudioPostViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AudioPostViewController.swift
+//  LambdaTimeline
+//
+//  Created by Jon Bash on 2020-01-14.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class AudioPostViewController: ShiftableViewController {
+    var postController: PostController!
+    var post: Post?
+}
