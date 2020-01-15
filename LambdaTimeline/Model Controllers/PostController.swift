3f8f2602e9e09cecf48a84e089d8ba63ea73746b
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-14 16:28:09 -0800

    Added fetchaudiooperation to fetch audio. Updated post controller to now create an audio post.

--- LambdaTimeline/Model Controllers/PostController.swift
+++ LambdaTimeline/Model Controllers/PostController.swift
@@ -46,6 +46,17 @@ class PostController {
         savePostToFirebase(post)
     }
 
+    func addAudioComment(with audioURL: URL, to post: inout Post) {
+
+        guard let currentUser = Auth.auth().currentUser,
+            let author = Author(user: currentUser) else { return }
+
+        let comment = Comment(text: nil, author: author, audioURL: audioURL)
+        post.comments.append(comment)
+
+        savePostToFirebase(post)
+    }
+
     func observePosts(completion: @escaping (Error?) -> Void) {
         
         postsRef.observe(.value, with: { (snapshot) in
