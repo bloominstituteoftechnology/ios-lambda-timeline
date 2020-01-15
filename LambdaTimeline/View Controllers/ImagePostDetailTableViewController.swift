92cd00d2f0c331bc8e1761eb944ca2c483d4439c
Jonalynn Masters <jonalynna.masters@gmail.com>
2020-01-14 20:24:18 -0800

    Day 2 initial commit

--- LambdaTimeline/View Controllers/ImagePostDetailTableViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostDetailTableViewController.swift
@@ -51,9 +51,13 @@ class ImagePostDetailTableViewController: UITableViewController {
                 self.tableView.reloadData()
             }
         }
+        let addVoiceComment = UIAlertAction(title: "Voice Comment", style: .default) { (_) in
+            self.performSegue(withIdentifier: "toAudioVC", sender: self)
+        }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
+        alert.addAction(addVoiceComment)
         alert.addAction(addCommentAction)
         alert.addAction(cancelAction)
         
