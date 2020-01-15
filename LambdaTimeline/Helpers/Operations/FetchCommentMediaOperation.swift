3e551298237c063000594a11fec65c290fb4c800
Jon Bash <jonbash@protonmail.com>
2020-01-14 18:42:10 -0800

    make fetchCommentMediaOp

--- LambdaTimeline/Helpers/Operations/FetchCommentMediaOperation.swift
+++ LambdaTimeline/Helpers/Operations/FetchCommentMediaOperation.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+class FetchCommentMediaOperation: ConcurrentOperation {
+
+    private(set) var comment: Comment
+    private(set) var mediaURL: URL
+    var mediaData: Data?
+
+    private let session: URLSession
+    private var dataTask: URLSessionDataTask?
+
+    init?(comment: Comment,
+         session: URLSession = URLSession.shared
+    ) {
+        guard case .audio(let url) = comment.content else { return nil }
+
+        self.mediaURL = url
+        self.comment = comment
+        self.session = session
+        
+        super.init()
+    }
+
+    override func start() {
+        state = .isExecuting
+
+        let task = session.dataTask(with: mediaURL) { data, response, error in
+            defer { self.state = .isFinished }
+            if self.isCancelled { return }
+            if let error = error {
+                NSLog("Error fetching data for \(self.comment): \(error)")
+                return
+            }
+
+            guard let data = data else {
+                NSLog("No data returned from fetch media operation data task.")
+                return
+            }
+
+            self.mediaData = data
+        }
+        task.resume()
+        dataTask = task
+    }
+
+    override func cancel() {
+        dataTask?.cancel()
+        super.cancel()
+    }
+}
