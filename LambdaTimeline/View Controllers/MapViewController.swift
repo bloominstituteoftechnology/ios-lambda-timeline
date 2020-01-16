09c0049b7dc0095c255abe8bb1d52993f6ee1c75
Jon Bash <jonbash@protonmail.com>
2020-01-16 14:44:39 -0800

    set up MapViewController

--- LambdaTimeline/View Controllers/MapViewController.swift
+++ LambdaTimeline/View Controllers/MapViewController.swift
@@ -21,5 +21,42 @@ class MapViewController: UIViewController {
         if let tabBarController = self.tabBarController as? TimelineTabBarController {
             self.postController = tabBarController.postController
         }
+        mapView.delegate = self
+        mapView.register(
+            MKMarkerAnnotationView.self,
+            forAnnotationViewWithReuseIdentifier: "PostAnnotationView")
+        setUpTags()
+    }
+
+    func setUpTags() {
+        let annotations = postController.posts.compactMap { $0.mapAnnotation }
+        mapView.addAnnotations(annotations)
+
+        guard let firstAnnotation = annotations.first else { return }
+        mapView.setRegion(
+            MKCoordinateRegion(
+                center: firstAnnotation.coordinate,
+                span: MKCoordinateSpan(
+                    latitudeDelta: 2,
+                    longitudeDelta: 2)),
+            animated: true)
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard
+            let postAnnotation = annotation as? PostAnnotation,
+            let annotationView = mapView
+                .dequeueReusableAnnotationView(withIdentifier: "PostAnnotationView")
+                as? MKMarkerAnnotationView
+            else { return nil }
+
+        annotationView.canShowCallout = true
+        let detailView = PostAnnotationView()
+        detailView.post = postAnnotation.post
+        annotationView.detailCalloutAccessoryView = detailView
+
+        return annotationView
     }
 }
