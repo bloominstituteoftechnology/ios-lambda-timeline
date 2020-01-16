c2641bd71fae3d5f129285f4109e54ae113eabbe
Jon Bash <jonbash@protonmail.com>
2020-01-15 18:23:46 -0800

    add beginnings of videoPostDVC

---
+++ LambdaTimeline/View Controllers/PostDetailVCs/VideoPostDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  VideoPostDetailViewController.swift
+//  LambdaTimeline
+//
+//  Created by Jon Bash on 2020-01-15.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoPostDetailViewController: PostDetailViewController {
+
+    @IBOutlet var videoPreviewView: VideoPreviewView!
+
+    override func updateViews() {
+        super.updateViews()
+        guard let data = mediaData else { return }
+        
+    }
+    
+}
