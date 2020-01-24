5b0c364a3a7e3a8815168a9542ca0192d3359a21
Robert Keffury <bobby.keffury@me.com>
2020-01-24 12:02:25 -0800

    Added MapKitViewController.

--- LambdaTimeline/View Controllers/MapViewController.swift
+++ LambdaTimeline/View Controllers/MapViewController.swift
@@ -7,25 +7,60 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
     //MARK: - Properties
     
+    private var userTrackingButton: MKUserTrackingButton!
+    private let locationManager = CLLocationManager()
+    var annotations: [PostAnnotation] = []
+    
+    private let annotationReuseIdentifier = "PostAnnotation"
+    
     //MARK: - Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSubviews()
     }
     
     //MARK: - Methods
     
-    //MARK: - Actions
+    private func setupSubviews() {
+        userTrackingButton = MKUserTrackingButton(mapView: mapView)
+        userTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+        mapView.addSubview(userTrackingButton)
+        
+        userTrackingButton.leftAnchor.constraint(equalTo: mapView.leftAnchor, constant: 20.0).isActive = true
+        userTrackingButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30.0).isActive = true
+    }
     
-    // MARK: - Navigation
+    func fetchAnnotations() {
+        
+    }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        fetchAnnotations()
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let post = annotation as? PostAnnotation else { return nil }
+        
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationReuseIdentifier, for: post) as? MKMarkerAnnotationView else {
+            fatalError("Missing registered map annotation view.")
+        }
+        
+        return annotationView
     }
 }
