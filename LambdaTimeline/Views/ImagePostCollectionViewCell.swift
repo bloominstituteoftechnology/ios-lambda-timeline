977c3a92afdc622a99b744c4ab20d6ef0adf0a42
John Kouris <jdkouris@gmail.com>
2020-01-23 19:15:50 -0800

    Display the location information of a post in the collectionViewCell

--- LambdaTimeline/Views/ImagePostCollectionViewCell.swift
+++ LambdaTimeline/Views/ImagePostCollectionViewCell.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ImagePostCollectionViewCell: UICollectionViewCell {
+    
+    var post: Post? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var labelBackgroundView: UIView!
+    
+    let locationBackground = UIView()
+    let locationName = UILabel()
     
     override func layoutSubviews() {
         super.layoutSubviews()
         setupLabelBackgroundView()
+        configureLocationNameLabelAndBackground()
     }
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -27,28 +43,54 @@ class ImagePostCollectionViewCell: UICollectionViewCell {
     
         titleLabel.text = post.title
         authorLabel.text = post.author.displayName
+        let location = CLLocation(latitude: post.latitude ?? 0, longitude: post.longitude ?? 0)
+        
+        fetchCityAndCountry(from: location) { (city, country, error) in
+            guard let city = city, let country = country, error == nil else { return }
+            DispatchQueue.main.async {
+                self.locationName.text = "\(city), \(country)"
+            }
+        }
+    }
+    
+    func fetchCityAndCountry(from location: CLLocation, completion: @escaping (_ city: String?, _ country:  String?, _ error: Error?) -> ()) {
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            completion(placemarks?.first?.locality, placemarks?.first?.country, error)
+        }
     }
 
     func setupLabelBackgroundView() {
         labelBackgroundView.layer.cornerRadius = 8
-//        labelBackgroundView.layer.borderColor = UIColor.white.cgColor
-//        labelBackgroundView.layer.borderWidth = 0.5
+        labelBackgroundView.layer.borderColor = UIColor.white.cgColor
+        labelBackgroundView.layer.borderWidth = 0.5
         labelBackgroundView.clipsToBounds = true
+    }
+    
+    func configureLocationNameLabelAndBackground() {
+        addSubview(locationBackground)
+        locationBackground.addSubview(locationName)
+        
+        locationBackground.translatesAutoresizingMaskIntoConstraints = false
+        locationName.translatesAutoresizingMaskIntoConstraints = false
+        
+        locationBackground.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+        locationName.textColor = .white
+        
+        NSLayoutConstraint.activate([
+            locationBackground.topAnchor.constraint(equalTo: self.topAnchor),
+            locationBackground.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            locationBackground.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            locationBackground.heightAnchor.constraint(equalToConstant: 40),
+            
+            locationName.centerYAnchor.constraint(equalTo: locationBackground.centerYAnchor),
+            locationName.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8)
+        ])
     }
     
     func setImage(_ image: UIImage?) {
         imageView.image = image
     }
 
-    var post: Post? {
-        didSet {
-            updateViews()
-        }
-    }
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var labelBackgroundView: UIView!
 
 }
