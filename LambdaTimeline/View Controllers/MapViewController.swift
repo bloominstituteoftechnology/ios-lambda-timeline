38b2c711a15f99c616714cc35098978e52c558ba
Robert Keffury <bobby.keffury@me.com>
2020-01-24 11:41:44 -0800

    PostAnnotation created.

---
+++ LambdaTimeline/View Controllers/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  LambdaTimeline
+//
+//  Created by Bobby Keffury on 1/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
