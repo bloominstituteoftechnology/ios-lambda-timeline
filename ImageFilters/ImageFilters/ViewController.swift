cb7b81e4a80120a22b7a41b534c1174f019c93b0
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-09 12:37:13 -0700

    added ImageFilter proj

---
+++ ImageFilters/ImageFilters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilters
+//
+//  Created by Jorge Alvarez on 3/9/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Test")
+    }
+
+
+}
+
