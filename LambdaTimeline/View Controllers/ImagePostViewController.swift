232c5f2c4d7f225b5c4552ec225c3b5104f12801
Enrique Gongora <EGTech24@gmail.com>
2020-04-06 15:55:33 -0700

    Added code for the filters. Still need to wire up the buttons

--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -27,6 +27,7 @@ class ImagePostViewController: ShiftableViewController {
     @IBOutlet weak var chooseImageButton: UIButton!
     @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var postButton: UIBarButtonItem!
+    @IBOutlet weak var slider: UISlider!
     
     // MARK: - Properties
     var postController: PostController!
@@ -35,6 +36,18 @@ class ImagePostViewController: ShiftableViewController {
     var context = CIContext(options: nil)
     var filterType: FilterType!
     
+    private var originalImage: UIImage? {
+        didSet {
+            // TODO: - Original Image -> Scaled Image
+        }
+    }
+    
+    private var scaledImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
     // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,17 +55,89 @@ class ImagePostViewController: ShiftableViewController {
         updateViews()
     }
     
+    // MARK: - Functions
+    
     func updateViews() {
-        guard let imageData = imageData,
-            let image = UIImage(data: imageData) else {
-                title = "New Post"
-                return
+        guard let imageData = imageData, let image = UIImage(data: imageData) else {
+            title = "New Post"
+            return
         }
         
         title = post?.title
         setImageViewHeight(with: image.ratio)
         imageView.image = image
         chooseImageButton.setTitle("", for: [])
+    }
+    
+    func filter(_ image: UIImage, for type: FilterType) -> UIImage? {
+        switch type {
+        case .sepia:
+            guard let cgImage = image.cgImage else { return nil }
+            let ciImage = CIImage(cgImage: cgImage)
+            let filter = CIFilter.sepiaTone()
+            filter.inputImage = ciImage
+            filter.intensity = slider.value
+            guard let outputCIImage = filter.outputImage else { return nil }
+            guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+            return UIImage(cgImage: outputCGImage)
+        case .blackAndWhite:
+            guard let cgImage = image.cgImage else { return nil }
+            let ciImage = CIImage(cgImage: cgImage)
+            let filter = CIFilter.photoEffectNoir()
+            filter.inputImage = ciImage
+            guard let outputCIImage = filter.outputImage else { return nil }
+            guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+            return UIImage(cgImage: outputCGImage)
+        case .blur:
+            guard let cgImage = image.cgImage else { return nil }
+            let ciImage = CIImage(cgImage: cgImage)
+            let filter = CIFilter.gaussianBlur()
+            filter.inputImage = ciImage
+            filter.radius = slider.value
+            guard let outputCIImage = filter.outputImage else { return nil }
+            guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+            return UIImage(cgImage: outputCGImage)
+        case .sharpen:
+            guard let cgImage = image.cgImage else { return nil }
+            let ciImage = CIImage(cgImage: cgImage)
+            let filter = CIFilter.sharpenLuminance()
+            filter.inputImage = ciImage
+            filter.sharpness = slider.value
+            guard let outputCIImage = filter.outputImage else { return nil }
+            guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+            return UIImage(cgImage: outputCGImage)
+        case .negative:
+            guard let cgImage = image.cgImage else { return nil }
+            let ciImage = CIImage(cgImage: cgImage)
+            let filter = CIFilter.colorInvert()
+            filter.inputImage = ciImage
+            guard let outputCIImage = filter.outputImage else { return nil }
+            guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+            return UIImage(cgImage: outputCGImage)
+        }
+    }
+    
+    // MARK: - Private Functions
+    
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            switch self.filterType {
+            case .sepia:
+                imageView.image = filter(scaledImage, for: .sepia)
+            case .blackAndWhite:
+                imageView.image = filter(scaledImage, for: .blackAndWhite)
+            case .blur:
+                imageView.image = filter(scaledImage, for: .blur)
+            case .sharpen:
+                imageView.image = filter(scaledImage, for: .sharpen)
+            case .negative:
+                imageView.image = filter(scaledImage, for: .negative)
+            case .none:
+                break
+            }
+        } else {
+            imageView.image = nil
+        }
     }
     
     private func presentImagePickerController() {
@@ -67,7 +152,7 @@ class ImagePostViewController: ShiftableViewController {
         imagePicker.delegate = self
         
         imagePicker.sourceType = .photoLibrary
-
+        
         present(imagePicker, animated: true, completion: nil)
     }
     
@@ -77,8 +162,8 @@ class ImagePostViewController: ShiftableViewController {
         
         guard let imageData = imageView.image?.jpegData(compressionQuality: 0.1),
             let title = titleTextField.text, title != "" else {
-            presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
-            return
+                presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
+                return
         }
         
         postController.createPost(with: title, ofType: .image, mediaData: imageData, ratio: imageView.image?.ratio) { (success) in
@@ -132,23 +217,15 @@ class ImagePostViewController: ShiftableViewController {
         
         view.layoutSubviews()
     }
-    
-
-    
 }
 
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
         chooseImageButton.setTitle("", for: [])
-        
         picker.dismiss(animated: true, completion: nil)
-        
         guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-        
         imageView.image = image
-        
         setImageViewHeight(with: image.ratio)
     }
     
