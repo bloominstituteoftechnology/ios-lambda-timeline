12700299cd8cf31a490774a7c52d3a62590def48
Chris <chris.gonzales.82@gmail.com>
2020-04-07 13:29:28 -0700

    Created model, model controller, and updated view controller

--- AudioComments2/AudioComments2/ViewControllers/AudioCommentViewController.swift
+++ AudioComments2/AudioComments2/ViewControllers/AudioCommentViewController.swift
@@ -7,28 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AudioCommentViewController: ViewController {
+    
+    // MARK: -Properties
+    
+    let audioCommentController = AudioCommentController()
+    var isRecording: Bool {
+        guard let audioRecorder = audioCommentController.audioRecorder else { return false}
+        return audioRecorder.isRecording
+    }
 
+    // MARK: -Outlets
     
     @IBOutlet weak var authorTextField: UITextField!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingTimeLabel: UILabel!
     
+    // MARK: -Actions
     
     @IBAction func recordTapped(_ sender: UIButton) {
     }
     @IBAction func cancelTapped(_ sender: UIButton) {
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
-    }
-    
-
    
 
 }
