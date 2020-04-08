86e0870009036abaa8de7357abf0e889613639b4
Keri Levesque <kerianne166@gmail.com>
2020-04-08 17:18:52 -0400

    added video view in storyboard, camera view controller and camera preview file

---
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CameraViewController.swift
+//  LambdaTimeline
+//
+//  Created by Keri Levesque on 4/8/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation // nothing will work without this
+
+class CameraViewController: UIViewController {
+
+    //MARK: Properties
+    
+    //MARK: Outlets
+    @IBOutlet weak var cameraPreview: CameraPreviewView!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    //MARK: View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
