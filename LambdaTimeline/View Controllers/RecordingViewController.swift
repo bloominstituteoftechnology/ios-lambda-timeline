9c12597f9cb39093aec73a7d644063ec6d621659
Enrique Gongora <EGTech24@gmail.com>
2020-04-08 15:19:16 -0700

    created new view controllers and files.

---
+++ LambdaTimeline/View Controllers/RecordingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordingViewController.swift
+//  LambdaTimeline
+//
+//  Created by Enrique Gongora on 4/8/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
