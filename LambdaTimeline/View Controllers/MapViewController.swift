c752610018be119f8bf7fc208d364d76df1c248a
Enrique Gongora <EGTech24@gmail.com>
2020-04-09 15:46:36 -0700

    Updated most model and view controllers

--- LambdaTimeline/View Controllers/MapViewController.swift
+++ LambdaTimeline/View Controllers/MapViewController.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var MKMapView: MKMapView!
+    
+    // MARK: - Variables
+    var postController: PostController!
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Functions
+    private func setupMapView() {
+        MKMapView.delegate = self
+        MKMapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "PostView")
     }
-    */
+    
+    private func fetchImagePosts() {
+        postController.observePosts { error in
+            if let error = error { print("Error: \(error)") }
+            DispatchQueue.main.async { self.MKMapView.addAnnotations(self.postController.posts) }
+        }
+    }
+    
+    private func fetchVideoPosts() {
+        postController.observeVideoPosts { error in
+            if let error = error { print("Error: \(error)") }
+            DispatchQueue.main.async { self.mapView.addAnnotations(self.postController.videoPosts) }
+        }
+    }
+}
 
+// MARK: - Extensions
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let _ = annotation as? Post {
+            guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "PostView") as? MKMarkerAnnotationView else { return nil }
+            annotationView.glyphImage = UIImage(systemName: "camera")
+            annotationView.glyphTintColor = .black
+            annotationView.markerTintColor = .systemBlue
+            return annotationView
+        } else if let _ = annotation as? VideoPost {
+            guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "PostView") as? MKMarkerAnnotationView else { return nil }
+            annotationView.glyphImage = UIImage(systemName: "video")
+            annotationView.glyphTintColor = .black
+            annotationView.markerTintColor = .systemRed
+            return annotationView
+        } else {
+            fatalError("Annotation object not supported")
+        }
+    }
 }
