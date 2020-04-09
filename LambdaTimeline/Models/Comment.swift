ced612bf4aab44b9c55efe3bb3dde0dbe062d838
Enrique Gongora <EGTech24@gmail.com>
2020-04-09 15:47:32 -0700

    Removed most errors from models

--- LambdaTimeline/Models/Comment.swift
+++ LambdaTimeline/Models/Comment.swift
@@ -11,39 +11,45 @@ import FirebaseAuth
 
 class Comment: FirebaseConvertible, Equatable {
     
-    static private let textKey = "text"
-    static private let author = "author"
-    static private let timestampKey = "timestamp"
+    private static let textKey = "text"
+    private static let author = "author"
+    private static let timestampKey = "timestamp"
+    private static let audioKey = "audio"
     
     let text: String
     let author: Author
     let timestamp: Date
+    var audio: String?
     
-    init(text: String, author: Author, timestamp: Date = Date()) {
+    init(text: String, author: Author, timestamp: Date = Date(), audio: String? = nil) {
         self.text = text
         self.author = author
         self.timestamp = timestamp
+        self.audio = audio
     }
     
-    init?(dictionary: [String : Any]) {
+    init?(dictionary: [String: Any]) {
         guard let text = dictionary[Comment.textKey] as? String,
             let authorDictionary = dictionary[Comment.author] as? [String: Any],
             let author = Author(dictionary: authorDictionary),
-            let timestampTimeInterval = dictionary[Comment.timestampKey] as? TimeInterval else { return nil }
+            let timestampTimeInterval = dictionary[Comment.timestampKey] as? TimeInterval,
+            let audio = dictionary[Comment.audioKey] as? String? else { return nil }
         
         self.text = text
         self.author = author
         self.timestamp = Date(timeIntervalSince1970: timestampTimeInterval)
+        self.audio = audio
     }
     
     var dictionaryRepresentation: [String: Any] {
-        return [Comment.textKey: text,
-                Comment.author: author.dictionaryRepresentation,
-                Comment.timestampKey: timestamp.timeIntervalSince1970]
+        [Comment.textKey: text,
+         Comment.author: author.dictionaryRepresentation,
+         Comment.timestampKey: timestamp.timeIntervalSince1970,
+         Comment.audioKey: audio as Any]
     }
     
-    static func ==(lhs: Comment, rhs: Comment) -> Bool {
-        return lhs.author == rhs.author &&
+    static func == (lhs: Comment, rhs: Comment) -> Bool {
+        lhs.author == rhs.author &&
             lhs.timestamp == rhs.timestamp
     }
 }
