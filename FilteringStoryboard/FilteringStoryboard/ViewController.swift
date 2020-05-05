19b75e847903230347e1f2f20ba58ff68ec318ba
Ezra Black <dev.ezrablack@gmail.com>
2020-05-04 23:29:38 -0500

    Added Base Features Project.

---
+++ FilteringStoryboard/FilteringStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilteringStoryboard
+//
+//  Created by Ezra Black on 5/4/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
