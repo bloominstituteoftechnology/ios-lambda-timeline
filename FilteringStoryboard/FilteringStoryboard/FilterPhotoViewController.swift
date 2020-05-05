aaac7ebfa86e7b2ebe90ec9b7aa13f6e944e0d42
Ezra Black <dev.ezrablack@gmail.com>
2020-05-05 00:28:29 -0500

    Added Base UI from Starter Project Added Image Helper Added Image Filter Project file

---
+++ FilteringStoryboard/FilteringStoryboard/FilterPhotoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FilterPhotoViewController.swift
+//  FilteringStoryboard
+//
+//  Created by Ezra Black on 5/5/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+class FilterPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+
