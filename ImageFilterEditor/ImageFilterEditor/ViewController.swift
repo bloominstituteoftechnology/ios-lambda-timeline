8fadf135cd20cb109751beb2cf1bfed6d64146c6
Mark Gerrior <github@gerrior.com>
2020-05-04 17:55:53 -0700

    Added ImageFilterEditor

---
+++ ImageFilterEditor/ImageFilterEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilterEditor
+//
+//  Created by Mark Gerrior on 5/4/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
