7233f2704ebbfaeaffb3e7f80a8d0ebd00af1564
Ezra Black <dev.ezrablack@gmail.com>
2020-05-06 16:31:13 -0500

    Added Base Video Project

---
+++ iOSVideoApp/iOSVideoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSVideoApp
+//
+//  Created by Ezra Black on 5/6/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
