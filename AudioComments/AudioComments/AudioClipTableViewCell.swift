b3fbfc8f3726a9c198316bc877c0f9ff124f7d18
Mark Gerrior <github@gerrior.com>
2020-05-05 23:16:54 -0700

    Added AudioClipTableViewCell

---
+++ AudioComments/AudioComments/AudioClipTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AudioClipTableViewCell.swift
+//  AudioComments
+//
+//  Created by Mark Gerrior on 5/5/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class AudioClipTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
