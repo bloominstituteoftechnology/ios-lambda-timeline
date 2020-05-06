706e86ba73a8140e02a95766fe3cbf5dde984cfe
Mark Gerrior <github@gerrior.com>
2020-05-05 17:13:56 -0700

    Wired up the cell

--- AudioComments/AudioComments/AudioTableViewCell.swift
+++ AudioComments/AudioComments/AudioTableViewCell.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class AudioTableViewCell: UITableViewCell {
 
+    // MARK: - Properties
     var recording: Recording? = nil
-    
+
+    // MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var currentTime: UILabel!
+    @IBOutlet weak var remainingTime: UILabel!
+    @IBOutlet weak var playPauseRecordStopButtonLabel: UIButton!
+
+    // MARK: - Actions
+    @IBAction func audioSlider(_ sender: Any) {
+    }
+
+    @IBAction func shareButton(_ sender: UIButton) {
+    }
+
+    @IBAction func backButton(_ sender: UIButton) {
+    }
+
+    @IBAction func playPauseRecordStopButton(_ sender: UIButton) {
+    }
+
+    @IBAction func forwardButton(_ sender: UIButton) {
+    }
+
+    @IBAction func deleteButton(_ sender: UIButton) {
+    }
+
 }
