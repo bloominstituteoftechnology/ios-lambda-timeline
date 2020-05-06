10039234735c3ba4a0b1373af4bc990781701b65
Ezra Black <dev.ezrablack@gmail.com>
2020-05-06 17:26:35 -0500

    Connected Boilerpl8 to collection View Controller

---
+++ iOSVideoApp/iOSVideoApp/views/VideoPostsCollectionViewController.swift
@@ -0,0 +1,133 @@
+//
+//  VideoPostsCollectionViewController.swift
+//  iOSVideoApp
+//
+//  Created by Ezra Black on 5/6/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+private let reuseIdentifier = "Cell"
+
+class VideoPostsCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Properties
+    
+    private func requestPermissionAndShowCamera() {
+            let status = AVCaptureDevice.authorizationStatus(for: .video)
+            
+            switch status {
+            case .notDetermined: // first time we've requested access
+                requestPermission()
+            case .restricted: // parental controls prevent user from camera/mic
+                fatalError("Tell user they need to request permission from parent")
+            case .denied:
+                fatalError("enable permissions in system preferences!")
+            case .authorized:
+                showCamera()
+            default:
+                fatalError("Handle new case for authorization due to deprication")
+            }
+        }
+        
+        private func requestPermission() {
+            AVCaptureDevice.requestAccess(for: .video) { (granted) in
+                guard granted else {
+                    fatalError("enable permissions in system preferences!")
+                }
+                DispatchQueue.main.async {
+                    self.showCamera()
+                }
+            }
+        }
+        
+        private func showCamera() {
+            performSegue(withIdentifier: "ShowCamera", sender: self)
+        }
+    
+
+    //MARK: - Actions
+
+    
+    @IBAction func recordButtonTapped(_ sender: Any) {
+        requestPermissionAndShowCamera()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
