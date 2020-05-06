b797251e8c71085032984a423dbe8d6da8516b19
Hector L <hectoremil21@hotmail.com>
2020-05-05 21:51:39 -0400

    Create custom cell for aaudio. Custom cocoa class. Outlets.

---
+++ LambdaTimeline/Views/AudioCommentTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AudioCommentTableViewCell.swift
+//  LambdaTimeline
+//
+//  Created by Karen Rodriguez on 5/5/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AudioCommentTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var authorLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
