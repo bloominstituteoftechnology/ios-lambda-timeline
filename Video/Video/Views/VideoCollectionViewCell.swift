4f03ec5a6b29fdee0573345d6ca95c09fcf5b232
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-06 18:15:19 -0400

    Created Collection view and cell

---
+++ Video/Video/Views/VideoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  VideoCollectionViewCell.swift
+//  Video
+//
+//  Created by Wyatt Harrell on 5/6/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+
+class VideoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var videoNameLabel: UILabel!
+}
