1debfb871d5b1fd878aa83bf154e548c738e554c
Mark Gerrior <github@gerrior.com>
2020-05-05 22:17:04 -0700

    Allow the user to create an audio comment in the Test UI.

--- AudioComments/AudioComments/View Controllers/RecordingViewController.swift
+++ AudioComments/AudioComments/View Controllers/RecordingViewController.swift
@@ -7,24 +7,92 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordingViewController: UIViewController {
+
+    // MARK: - Properites
+
+    var recordingURL: URL?
+    var audioRecorder: AVAudioRecorder?
+
+    // MARK: - Actions
+
+    @IBAction func recordButton(_ sender: Any) {
+        recordingURL = createNewRecordingURL()
+
+        guard let recordingURL = recordingURL else { return }
+
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        audioRecorder = try? AVAudioRecorder(url: recordingURL, format: format) // TODO: Error handling do/catch
+        audioRecorder?.delegate = self
+        audioRecorder?.record()
+    }
+
+    @IBAction func stopButton(_ sender: Any) {
+        audioRecorder?.stop()
+    }
+
+    @IBAction func cancelButton(_ sender: Any) {
+        audioRecorder?.stop()
+        let success = audioRecorder?.deleteRecording()
+        if let success = success {
+            if success {
+                print("Recording Canceled")
+            } else {
+                print("Failed to Cancel Recording.")
+            }
+        } else {
+            print("Unabled to Cancel Recording.")
+        }
+    }
+
+    @IBAction func sendButton(_ sender: Any) {
+        guard let recordingURL = recordingURL else { return }
+
+        print("Send: \(recordingURL)")
+    }
+
+    // MARK: - Outlets
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Private
+    private func createNewRecordingURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+
+        return file
     }
-    */
+}
 
+extension RecordingViewController: AVAudioRecorderDelegate {
+
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag,
+            let recordingURL = recordingURL {
+            let playbackAudioPlayer = try? AVAudioPlayer(contentsOf: recordingURL) // TODO: Error handling
+
+            if let _ = playbackAudioPlayer {
+                print("Saved recording to \(recordingURL)")
+            } else {
+                print("Nothing to playback")
+            }
+
+            // Dispose of recorder (otherwise I can still cancel and it will delete the recording!)
+            audioRecorder = nil
+        }
+    }
+
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            print("Audio Record Error: \(error)")
+        }
+    }
 }
