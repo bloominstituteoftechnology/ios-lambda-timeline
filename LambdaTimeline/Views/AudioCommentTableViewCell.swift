c17949b4e867b0a0b4b2b4c2916b7cc391962452
Hector L <hectoremil21@hotmail.com>
2020-05-06 00:36:25 -0400

    Close but no cigar. When fetching comments from firebase, audioURL is empty.

--- LambdaTimeline/Views/AudioCommentTableViewCell.swift
+++ LambdaTimeline/Views/AudioCommentTableViewCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseStorage
+import AVFoundation
 
 class AudioCommentTableViewCell: UITableViewCell {
 
@@ -23,6 +24,8 @@ class AudioCommentTableViewCell: UITableViewCell {
 
     var audioURL: String?
 
+    var timer: Timer?
+
     // MARK: - Outlets
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var playButton: UIButton!
@@ -31,6 +34,9 @@ class AudioCommentTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         playButton.tintColor = .gray
+        updateViews()
+        try? prepareAudioSession()
+        loadSound()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -39,48 +45,110 @@ class AudioCommentTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    deinit {
+        timer?.invalidate()
+    }
+
     private func loadSound() {
         guard let url = audioURL else { return }
         let httpsReference = Storage.storage().reference(forURL: url)
+        let localURL = createLocalURL()
 
+        let downloadTask = httpsReference.write(toFile: localURL) { url, error in
+            if let error = error {
+                NSLog("Error doing loading of soudn idk HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE: \(error)")
+                return
+            }
+
+            guard let url = url else {
+                NSLog("error unqrapping url")
+                return
+            }
+            DispatchQueue.main.async {
+                do {
+                    self.audioPlayer = try AVAudioPlayer(contentsOf: url)
+                    self.playButton.tintColor = .blue
+                } catch {
+                    NSLog("Error assigning audioplayer with unwrapped url \(error)")
+                }
+            }
+        }
+
+        downloadTask.observe(.resume) { snapshot in
+            print("stuff happeningggggggg")
+        }
+    }
+
+    // MARK: - Timer
+
+    func startTimer() {
+        timer?.invalidate()
+
+        timer = Timer.scheduledTimer(withTimeInterval: 0.030, repeats: true) { [weak self] (_) in
+            guard let self = self else { return }
+
+            self.updateViews()
+        }
+    }
+
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    // MARK: - Recording
+
+    func createLocalURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+
+        print("recording URL: \(file)")
+
+        return file
     }
 
     // MARK: - Playback
 
-//    func loadAudio() {
-//        let songURL = Bundle.main.url(forResource: "piano", withExtension: "mp3")!
-//
-//        audioPlayer = try? AVAudioPlayer(contentsOf: songURL)
-//        audioPlayer?.isMeteringEnabled = true
-//    }
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+        try session.setActive(true, options: []) // can fail if on a phone call, for instance
+    }
 
-//    func prepareAudioSession() throws {
-//        let session = AVAudioSession.sharedInstance()
-//        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
-//        try session.setActive(true, options: []) // can fail if on a phone call, for instance
-//    }
-//
-//    func play() {
-//        audioPlayer?.play()
-//    }
-//
-//    func pause() {
-//        audioPlayer?.pause()
-//    }
-//
-//    func updateViews() {
-//        playButton.isSelected = isPlaying
-//    }
+    func play() {
+        audioPlayer?.play()
+    }
+
+    func pause() {
+        audioPlayer?.pause()
+    }
+
+    func updateViews() {
+        playButton.isSelected = isPlaying
+    }
+
+    @IBAction func playButtonTouched(_ sender: Any) {
+        if isPlaying {
+            pause()
+            updateViews()
+        } else {
+            play()
+            updateViews()
+        }
+    }
+
 }
-//
-//extension AudioCommentTableViewCell: AVAudioPlayerDelegate {
-//    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-//        updateViews()
-//    }
-//    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
-//        if let error = error {
-//            print("Audio Player Decode Error: \(error)")
-//        }
-//        updateViews()
-//    }
-//}
+
+extension AudioCommentTableViewCell: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+    }
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("Audio Player Decode Error: \(error)")
+        }
+        updateViews()
+    }
+}
