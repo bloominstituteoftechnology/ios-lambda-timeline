43b13930d6491e25dee53b738860eccec49f9cb9
Mark Gerrior <github@gerrior.com>
2020-05-05 21:22:44 -0700

    Added RecordingViewController

---
+++ AudioComments/AudioComments/View Controllers/RecordingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecordingViewController.swift
+//  AudioComments
+//
+//  Created by Mark Gerrior on 5/5/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class RecordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
