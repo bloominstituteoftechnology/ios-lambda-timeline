d0df57123f985ce1906dcd4f8fbf14e34b024d00
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-06 14:10:08 -0400

    Created ImageFilterEditor project to test Image filters and UI setup

---
+++ ImageFilterEditor/ImageFilterEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageFilterEditor
+//
+//  Created by Joshua Rutkowski on 5/6/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
