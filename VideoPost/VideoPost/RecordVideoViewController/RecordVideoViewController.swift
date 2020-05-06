e75dd972bbe80b2b9b7dd748ef6884ef25551b0e
Shawn Gee <shawngee@gmail.com>
2020-05-06 16:40:51 -0400

    Added recordVideoViewController and CameraPreviewView

---
+++ VideoPost/VideoPost/RecordVideoViewController/RecordVideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecordVideoViewController.swift
+//  VideoPost
+//
+//  Created by Shawn Gee on 5/6/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class RecordVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
