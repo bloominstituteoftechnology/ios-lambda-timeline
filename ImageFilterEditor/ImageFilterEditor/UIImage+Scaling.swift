ec62a09d40831375ce8eb584abe441765d569a4d
Gerardo <gahernandez02@gmail.com>
2020-05-06 16:02:22 -0700

    Added ImagePostTableViewController skeleton and storyboard elements

---
+++ ImageFilterEditor/ImageFilterEditor/UIImage+Scaling.swift
@@ -0,0 +1,40 @@
+//
+//  UIImage+Scaling.swift
+//  ImageFilterEditor
+//
+//  Created by Gerardo Hernandez on 5/6/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    /// Resize the image to a max dimension from size parameter
+    func imageByScaling(toSize size: CGSize) -> UIImage? {
+        
+        guard size.width > 0 && size.height > 0 else { return nil }
+        
+        let originalAspectiRatio = self.size.width/self.size.height
+        var correctedSize = size
+        
+        if correctedSize.width > correctedSize.width*originalAspectiRatio {
+            correctedSize.width = correctedSize.width*originalAspectiRatio
+        } else {
+            correctedSize.height = correctedSize.height/originalAspectiRatio
+        }
+        
+        return UIGraphicsImageRenderer(size: correctedSize, format: imageRendererFormat).image { context in
+            draw(in: CGRect(origin: .zero, size: correctedSize))
+        }
+    }
+    
+    /// Renders the image if the pixel data was rotated due to orientation of camera
+
+    var flatted: UIImage {
+        if imageOrientation == .up { return self }
+        return UIGraphicsImageRenderer(size: size, format: imageRendererFormat).image { context in
+            draw(at: .zero)
+        }
+    }
+}
