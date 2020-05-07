cb21f01122d74734aab3cafec875f70bdf834150
David Wright <davidtwright89@gmail.com>
2020-05-06 22:04:53 -0400

    Created new Xcode project to use as a playground for Core Image filters and UI setup

---
+++ ImageFilterEditor/ImageFilterEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilterEditor
+//
+//  Created by David Wright on 5/6/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
