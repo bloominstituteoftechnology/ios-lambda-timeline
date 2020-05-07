9af5503871c659c6d0582fc005268f3448c5873c
Mark Gerrior <github@gerrior.com>
2020-05-06 20:04:13 -0700

    Added ClipCollectionViewCell. Project organizaiton.

---
+++ VideoPost/VideoPost/ClipCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ClipCollectionViewCell.swift
+//  VideoPost
+//
+//  Created by Mark Gerrior on 5/6/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ClipCollectionViewCell: UICollectionViewCell {
+    
+}
