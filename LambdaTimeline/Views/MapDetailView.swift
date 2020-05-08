7bd4e44a20770db5cd89c1706e6bc34c716bb991
Mark Gerrior <github@gerrior.com>
2020-05-07 21:33:22 -0700

    Flushed out MapDetailView

--- LambdaTimeline/Views/MapDetailView.swift
+++ LambdaTimeline/Views/MapDetailView.swift
@@ -10,12 +10,63 @@ import UIKit
 
 class MapDetailView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Properties
+    var post: Post? {
+        didSet {
+            updateSubviews()
+        }
     }
-    */
 
+    private let titleLabel = UILabel()
+    private let authorLabel = UILabel()
+    private let latitudeLabel = UILabel()
+    private let longitudeLabel = UILabel()
+
+    private lazy var latLonFormatter: NumberFormatter = {
+        let result = NumberFormatter()
+        result.numberStyle = .decimal
+        result.minimumIntegerDigits = 1
+        result.minimumFractionDigits = 2
+        result.maximumFractionDigits = 2
+        return result
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        latitudeLabel.setContentHuggingPriority(.defaultLow+1, for: .horizontal)
+
+        let placeDateStackView = UIStackView(arrangedSubviews: [titleLabel, authorLabel])
+        placeDateStackView.spacing = UIStackView.spacingUseSystem
+        let latLonStackView = UIStackView(arrangedSubviews: [latitudeLabel, longitudeLabel])
+        latLonStackView.spacing = UIStackView.spacingUseSystem
+        let mainStackView = UIStackView(arrangedSubviews: [placeDateStackView, latLonStackView])
+        mainStackView.axis = .vertical
+        mainStackView.spacing = UIStackView.spacingUseSystem
+
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(mainStackView)
+        mainStackView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        mainStackView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        mainStackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    // MARK: - Private
+
+    private func updateSubviews() {
+        guard let post = post else { return }
+
+        let latitude = post.latitude ?? 0.0
+        let longitude = post.longitude ?? 0.0
+
+        titleLabel.text = post.title ?? "Untitled"
+        authorLabel.text = post.author.displayName
+        latitudeLabel.text = "Lat: " + latLonFormatter.string(from: latitude as NSNumber)!
+        longitudeLabel.text = "Lon: " + latLonFormatter.string(from: longitude as NSNumber)!
+    }
 }
