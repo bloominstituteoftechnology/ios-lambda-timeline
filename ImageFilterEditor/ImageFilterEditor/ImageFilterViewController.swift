0109ba608642f292535f885d597d70e5a3de1354
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-07 21:26:54 -0400

    Added image filtering for sliders

--- ImageFilterEditor/ImageFilterEditor/ImageFilterViewController.swift
+++ ImageFilterEditor/ImageFilterEditor/ImageFilterViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreImage
+import Photos
+import CoreImage.CIFilterBuiltins
 
 class ImageFilterViewController: UIViewController {
-
+    
     // MARK: - Outlets
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var saturationSlider: UISlider!
@@ -18,21 +21,53 @@ class ImageFilterViewController: UIViewController {
     @IBOutlet var exposureSlider: UISlider!
     
     // MARK: - Properties
+    var originalImage: UIImage? {
+        didSet {
+            guard let originalImage = originalImage else { return }
+            
+            var scaledSize = imageView.bounds.size
+            let scale: CGFloat = 1 // UIScreen.main.scale
+            
+            scaledSize = CGSize(width: scaledSize.width*scale,
+                                height: scaledSize.height*scale)
+            
+            guard let scaledUIImage = originalImage.imageByScaling(toSize: scaledSize) else { return }
+            
+            scaledImage = CIImage(image: scaledUIImage)
+        }
+    }
+    
+    var scaledImage: CIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    private let context = CIContext()
+    private let colorControlsFilter = CIFilter.colorControls()
+    private let exposureFilter = CIFilter.exposureAdjust()
+    private let invertColorsFilter = CIFilter.colorInvert()
+    private let greyscaleFilter = CIFilter.photoEffectNoir()
     
     
     // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    
-    // MARK: - Actions
-    @IBAction func choosePhoto(_ sender: UIBarButtonItem) {
+        
+        originalImage = imageView.image
         
     }
     
+    
+    // MARK: - Actions
+    @IBAction func choosePhoto(_ sender: UIBarButtonItem) {
+        presentImagePickerController()
+    }
+    
     @IBAction func invertColors(_ sender: UIButton) {
+        guard let inputImage = scaledImage else { return }
+        invertColorsFilter.inputImage = inputImage
+        
         
     }
     
@@ -41,9 +76,78 @@ class ImageFilterViewController: UIViewController {
     }
     
     @IBAction func savePhoto(_ sender: UIButton) {
+        guard let originalImage = originalImage?.flattened, let ciImage = CIImage(image: originalImage) else { return }
         
+        let processedImage = self.image(byFiltering: ciImage)
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            guard status == .authorized else { return }
+            
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetCreationRequest.creationRequestForAsset(from: processedImage)
+            }) { (success, error) in
+                if let error = error {
+                    print("Error saving photo: \(error)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.presentSuccessfulSaveAlert()
+                }
+            }
+        }
     }
     
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateImage()
+    }
+    
+    
     // MARK: - Methods
+    private func image(byFiltering inputImage: CIImage) -> UIImage {
+        
+        colorControlsFilter.inputImage = inputImage
+        colorControlsFilter.saturation = saturationSlider.value
+        colorControlsFilter.brightness = brightnessSlider.value
+        colorControlsFilter.contrast = contrastSlider.value
+        
+        exposureFilter.inputImage = colorControlsFilter.outputImage
+        exposureFilter.ev = exposureSlider.value
+        
+        guard let outputImage = exposureFilter.outputImage else { return originalImage! }
+        
+        guard let renderedImage = context.createCGImage(outputImage, from: outputImage.extent) else { return originalImage! }
+        
+        return UIImage(cgImage: renderedImage)
+    }
+    
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            imageView.image = image(byFiltering: scaledImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("The photo library is not available.")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func presentSuccessfulSaveAlert() {
+        let alert = UIAlertController(title: "Photo Saved!", message: "The photo has been saved to your Photo Library!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 }
+
+
 
---
+++ ImageFilterEditor/ImageFilterEditor/ImageProcessing.swift
@@ -0,0 +1,52 @@
+//
+//  ImageProcessing.swift
+//  ImageFilterEditor
+//
+//  Created by Christy Hicks on 5/7/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+extension ImageFilterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension UIImage {
+    func imageByScaling(toSize size: CGSize) -> UIImage? {
+        guard size.width > 0 && size.height > 0 else { return nil }
+        
+        let originalAspectRatio = self.size.width/self.size.height
+        var correctedSize = size
+        
+        if correctedSize.width > correctedSize.width*originalAspectRatio {
+            correctedSize.width = correctedSize.width*originalAspectRatio
+        } else {
+            correctedSize.height = correctedSize.height/originalAspectRatio
+        }
+        
+        return UIGraphicsImageRenderer(size: correctedSize, format: imageRendererFormat).image { context in
+            draw(in: CGRect(origin: .zero, size: correctedSize))
+        }
+    }
+    
+    var flattened: UIImage {
+        if imageOrientation == .up { return self }
+        return UIGraphicsImageRenderer(size: size, format: imageRendererFormat).image { context in
+            draw(at: .zero)
+        }
+    }
+}
