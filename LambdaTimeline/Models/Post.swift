d4078c420e87da88603daf2c31d0ac957dd07122
Mark Gerrior <github@gerrior.com>
2020-05-07 21:32:23 -0700

    Added long and lat to Post

--- LambdaTimeline/Models/Post.swift
+++ LambdaTimeline/Models/Post.swift
@@ -8,20 +8,29 @@
 
 import Foundation
 import FirebaseAuth
+import MapKit
 
 enum MediaType: String {
     case image
 }
 
-class Post {
-    
-    init(title: String, mediaURL: URL, ratio: CGFloat? = nil, author: Author, timestamp: Date = Date()) {
+class Post: NSObject {
+
+    init(title: String,
+         mediaURL: URL,
+         ratio: CGFloat? = nil,
+         author: Author,
+         timestamp: Date = Date(),
+         latitude: Double? = nil,
+         longitude: Double? = nil) {
         self.mediaURL = mediaURL
         self.ratio = ratio
         self.mediaType = .image
         self.author = author
         self.comments = [Comment(text: title, author: author)]
         self.timestamp = timestamp
+        self.latitude = latitude
+        self.longitude = longitude
     }
     
     init?(dictionary: [String : Any], id: String) {
@@ -41,6 +50,8 @@ class Post {
         self.timestamp = Date(timeIntervalSince1970: timestampTimeInterval)
         self.comments = captionDictionaries.compactMap({ Comment(dictionary: $0) })
         self.id = id
+        self.latitude = dictionary[Post.latitudeKey] as? Double
+        self.longitude = dictionary[Post.longitudeKey] as? Double
     }
     
     var dictionaryRepresentation: [String : Any] {
@@ -51,9 +62,13 @@ class Post {
                 Post.timestampKey: timestamp.timeIntervalSince1970]
         
         guard let ratio = self.ratio else { return dict }
-        
+        guard let latitude = self.latitude else { return dict }
+        guard let longitude = self.longitude else { return dict }
+
         dict[Post.ratioKey] = ratio
-        
+        dict[Post.latitudeKey] = latitude
+        dict[Post.longitudeKey] = longitude
+
         return dict
     }
     
@@ -64,6 +79,8 @@ class Post {
     var comments: [Comment]
     var id: String?
     var ratio: CGFloat?
+    var latitude: Double?
+    var longitude: Double?
     
     var title: String? {
         return comments.first?.text
@@ -76,4 +93,14 @@ class Post {
     static private let commentsKey = "comments"
     static private let timestampKey = "timestamp"
     static private let idKey = "id"
+    static private let latitudeKey = "latitude"
+    static private let longitudeKey = "longitude"
+}
+
+extension Post: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        let lat = latitude ?? 0.0
+        let long = longitude ?? 0.0
+        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
 }
---
+++ LambdaTimeline/Views/MapDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  MapDetailView.swift
+//  LambdaTimeline
+//
+//  Created by Mark Gerrior on 5/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MapDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
