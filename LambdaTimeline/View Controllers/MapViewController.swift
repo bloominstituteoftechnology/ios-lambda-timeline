47845c3a582e2645c62044e3010fa5fdc5e1f835
Mark Gerrior <github@gerrior.com>
2020-05-07 20:24:31 -0700

    Got the map on the screen

--- LambdaTimeline/View Controllers/MapViewController.swift
+++ LambdaTimeline/View Controllers/MapViewController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+// NOTE: Don't forget Project > Target > General > Frameworks > add MapKit
+import MapKit
 
 class MapViewController: UIViewController {
+
+    // MARK: - Properites
+
+    // FIXME: Post object goes here
+
+    // MARK: - Actions
+
+
+    // MARK: - Outlets
+
+    @IBOutlet var mapView: MKMapView!
+
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+
+        // MKMarkerAnnotationView like a table view cell
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "PostView")
+
+//        print("Total posts: \(posts.count)")
+//
+//        for post in posts {
+//
+//            DispatchQueue.main.async {
+//                self.mapView.addAnnotations(posts)
+//
+//                // FIXME: Only 1? What about the rest?
+//                guard let posts = posts.first else { return }
+//
+//                let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
+//                let region = MKCoordinateRegion(center: posts.coordinate, span: span)
+//                self.mapView.setRegion(region, animated: true)
+//            }
+//        }
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Private
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        guard let post = annotation as? Post else {
+//            fatalError("Only posts are supported")
+//        }
+
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "PostView", for: annotation) as? MKMarkerAnnotationView else {
+            fatalError("Missing a registered view")
+        }
+
+//        annotationView.canShowCallout = true
+//        let detailView = QuakeDetailView()
+//        detailView.quake = quake
+//        annotationView.detailCalloutAccessoryView = detailView
+
+        return annotationView
     }
-    */
-
 }
