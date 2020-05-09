4d19aaa555e08ef06a356e5a42cf78f9e9ecb3fc
Christian Lorenzo <clbmiami2004@gmail.com>
2020-05-09 11:14:00 -0700

    Added ImageFilterProject
    Added project into Lambda's Timeline

---
+++ ImageFilterEditor/ImageFilterEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilterEditor
+//
+//  Created by Christian Lorenzo on 5/6/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImageFilterEditor/View/EffectCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EffectCollectionViewCell.swift
+//  ImageFilterEditor
+//
+//  Created by Christian Lorenzo on 5/7/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class EffectCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - IBOulets
+    
+    @IBOutlet weak var effectImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
