a593311ebd323156bca1ab40fa543267b0748b69
Jessie <jessie.griffin@icloud.com>
2020-05-08 22:29:12 -0400

    Adds ViewController.swift files for TableView and TableViewCell

---
+++ AudioComments/AudioComments/AudioCommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AudioCommentTableViewCell.swift
+//  AudioComments
+//
+//  Created by Jessie Ann Griffin on 5/8/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class AudioCommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
