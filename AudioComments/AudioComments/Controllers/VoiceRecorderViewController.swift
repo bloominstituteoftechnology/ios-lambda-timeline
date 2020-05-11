21eaf32d0d1e385b4744bacd0c6f74d9ab3e1c70
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-11 16:01:10 -0400

    Created View Controllers: VoiceRecorderViewController and VoiceCommentsTableViewController

---
+++ AudioComments/AudioComments/Controllers/VoiceRecorderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VoiceRecorderViewController.swift
+//  AudioComments
+//
+//  Created by Joshua Rutkowski on 5/10/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class VoiceRecorderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
