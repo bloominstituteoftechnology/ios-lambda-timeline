1be14c8a89b150a95188e6fb9b8c34cb87f8d3d8
David Wright <davidtwright89@gmail.com>
2020-05-10 23:02:26 -0400

    Deleted the default ViewController file and created a new AudioRecorderController file

---
+++ AudioComments/AudioComments/View Controllers/AudioRecorderController.swift
@@ -0,0 +1,30 @@
+//
+//  AudioRecorderController.swift
+//  AudioComments
+//
+//  Created by David Wright on 5/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecorderController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
