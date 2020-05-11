d623d39d98132c8463c7f5e4589407f1285a2900
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-11 15:58:08 -0400

    Updated AppDelegate and created CoreData model

--- AudioComments/AudioComments/AppDelegate.swift
+++ AudioComments/AudioComments/AppDelegate.swift
@@ -2,16 +2,29 @@
 //  AppDelegate.swift
 //  AudioComments
 //
-//  Created by Joshua Rutkowski on 5/11/20.
+//  Created by Joshua Rutkowski on 5/10/20.
 //  Copyright © 2020 Josh Rutkowski. All rights reserved.
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    lazy var persistentContainer: NSPersistentContainer = {
 
+        let container = NSPersistentContainer(name:"Audio")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
 
+                fatalError("Unresolved error, \((error as NSError).userInfo)")
+            }
+        })
+        return container
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,6 +44,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+      // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+      // Saves changes in the application's managed object context before the application terminates.
+      self.saveContext()
+    }
+    
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
 
 
 }
