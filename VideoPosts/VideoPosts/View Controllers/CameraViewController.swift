571fd80538c32cd7e8bdeaa16d0206e2aec46aaa
David Wright <davidtwright89@gmail.com>
2020-05-13 00:16:20 -0400

    Implemented functionality in CameraViewController to allow the user to record a video and save it to their documents directory

--- VideoPosts/VideoPosts/View Controllers/CameraViewController.swift
+++ VideoPosts/VideoPosts/View Controllers/CameraViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import AVKit
 
 class CameraViewController: UIViewController {
 
+    // Properties
+    
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    
+    private var player: AVPlayer!
+    
     // IBOutlets
     
     @IBOutlet var recordButton: UIButton!
@@ -18,14 +26,125 @@ class CameraViewController: UIViewController {
     // IBActions
     
     @IBAction func recordButtonPressed(_ sender: UIButton) {
-        
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
     }
     
     // View Controller Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cameraView.videoPlayerLayer.videoGravity = .resizeAspectFill
+        setupCamera()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
+    
+    // Methods
+    
+    private func setupCamera() {
+        let camera = bestCamera()
+        let microphone = bestMicrophone()
+        
+        captureSession.beginConfiguration() // begin configuring session
+        
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            preconditionFailure("Can't create an input from the camera, but we should do something better than crashing!")
+        }
+        
+        guard let microphoneInput = try? AVCaptureDeviceInput(device: microphone) else {
+            preconditionFailure("Can't create an input from the microphone, but we should do something better than crashing!")
+        }
+        
+        guard captureSession.canAddInput(cameraInput) else {
+            preconditionFailure("This session can't handle this type of input: \(cameraInput)")
+        }
+        
+        guard captureSession.canAddInput(microphoneInput) else {
+            preconditionFailure("This session can't handle this type of input: \(microphoneInput)")
+        }
+        
+        captureSession.addInput(cameraInput)
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        guard captureSession.canAddOutput(fileOutput) else {
+            preconditionFailure("This session can't handle this type of output: \(fileOutput)")
+        }
+        
+        captureSession.addOutput(fileOutput)
+        
+        captureSession.commitConfiguration() // finished configuring session
+        
+        cameraView.session = captureSession
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        preconditionFailure("No cameras on device match the specs that we need")
+    }
+    
+    private func bestMicrophone() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(for: .audio) {
+            return device
+        }
+        
+        preconditionFailure("No microphones on device match the specs that we need")
+    }
+    
+    /// Creates a new file URL in the documents directory
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 
-        // Do any additional setup after loading the view.
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+    
+    private func updateViews() {
+        recordButton.isSelected = fileOutput.isRecording
+    }
+}
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        updateViews()
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving video: \(error)")
+        }
+        
+        print("Video URL: \(outputFileURL)")
+        updateViews()
+        
+        // TODO: Playback saved recording
     }
 }
