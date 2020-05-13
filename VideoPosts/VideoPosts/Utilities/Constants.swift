c71271e8f0eeb03bdf5f887110022852932deec7
David Wright <davidtwright89@gmail.com>
2020-05-12 23:12:27 -0400

    Created a Utilities folder with a Constants.swift file in it. Added a segue identifier to the Constants file.

---
+++ VideoPosts/VideoPosts/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  VideoPosts
+//
+//  Created by David Wright on 5/12/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+struct Segues {
+    static let showCameraIdentifier = "ShowCamera"
+}
