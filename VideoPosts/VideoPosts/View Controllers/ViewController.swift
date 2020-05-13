2bd18490706f4abb315f486b1e26719ab2671e08
David Wright <davidtwright89@gmail.com>
2020-05-12 23:24:19 -0400

    Implemented the initial ViewController to handle the camera and microphone permissions

--- VideoPosts/VideoPosts/View Controllers/ViewController.swift
+++ VideoPosts/VideoPosts/View Controllers/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        requestPermissionAndShowCamera()
+    }
+    
+    private func requestPermissionAndShowCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            requestVideoPermission()
+            
+        case .restricted: // Parental controls, for instance, are preventing recording
+            preconditionFailure("Video is disabled, please review device restrictions")
+            
+        case .denied:
+            preconditionFailure("Tell the user they can't use the app without giving permissions via Settings > Privacy > Video")
+            
+        case .authorized:
+            showCamera()
+        
+        @unknown default:
+            preconditionFailure("A new status code was added that we need to handle")
+        }
     }
 
-
+    private func requestVideoPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { isGranted in
+            guard isGranted else {
+                preconditionFailure("UI: Tell the user to enable permissions for Video/Camera")
+            }
+            
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
+    
+    private func showCamera() {
+        performSegue(withIdentifier: SegueIdentifiers.showCamera, sender: self)
+    }
 }
 
