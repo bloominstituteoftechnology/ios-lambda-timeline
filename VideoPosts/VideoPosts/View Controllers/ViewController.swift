00c8f8e6163f8e3071a527ea8c19f698c3aa7cbb
David Wright <davidtwright89@gmail.com>
2020-05-13 00:47:57 -0400

    Added helpful comments in the ViewController file

--- VideoPosts/VideoPosts/View Controllers/ViewController.swift
+++ VideoPosts/VideoPosts/View Controllers/ViewController.swift
@@ -19,20 +19,21 @@ class ViewController: UIViewController {
     
     private func requestPermissionAndShowCamera() {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .notDetermined:
+            
+        case .notDetermined: // 1st run and the user hasn't been asked to give permission
             requestVideoPermission()
             
         case .restricted: // Parental controls, for instance, are preventing recording
             preconditionFailure("Video is disabled, please review device restrictions")
             
-        case .denied:
+        case .denied: // 2nd+ run, the user didn't trust us, or they said no by accident (show how to enable)
             preconditionFailure("Tell the user they can't use the app without giving permissions via Settings > Privacy > Video")
             
-        case .authorized:
+        case .authorized: // 2nd+ run, the user has given the app permission to use the camera
             showCamera()
         
         @unknown default:
-            preconditionFailure("A new status code was added that we need to handle")
+            preconditionFailure("A new status code for AVCaptureDevice authorization was added that we need to handle")
         }
     }
 
