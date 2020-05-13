c69ebb160d455f280f248f0b36ae9c8220e8dc4d
Alex Thompson <guitarman226@gmail.com>
2020-05-13 01:10:19 -0500

    Added a model to store some fakeData to test how my custom cell would look before i went any further with my code, had to get some constaints set up in the .xib cell that was fun, will continue tomorrow

--- AudioComments/AudioComments/AudioRecorderController.swift
+++ AudioComments/AudioComments/AudioRecorderController.swift
@@ -11,11 +11,13 @@ import AVFoundation
 
 class AudioRecorderController: UIViewController {
     
+    var recording = Recording(name: "Alex")
+    
     var audioPlayer: AVAudioPlayer? {
         didSet {
             guard let audioPlayer = audioPlayer else { return }
             
-//            audioPlayer.delegate = self
+            audioPlayer.delegate = self
             
         }
     }
@@ -23,12 +25,19 @@ class AudioRecorderController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        recordings.append(recording)
+        tableView.register(UINib(nibName: "AudioCommentTableViewCell", bundle: nil), forCellReuseIdentifier: "AudioCell")
+        
     }
     
     weak var timer: Timer?
     
     var recordingURL: URL?
     var audioRecorder: AVAudioRecorder?
+    
+    var recordings: [Recording] = []
+    
+    
     
     var isPlaying: Bool {
         audioPlayer?.isPlaying ?? false
@@ -103,8 +112,91 @@ class AudioRecorderController: UIViewController {
             }
         case .denied:
             print("Microphone acces has been blocked!")
-        default:
-            <#code#>
+            
+            let alertController = UIAlertController(title: "Microphone access denied 😭", message: "Please allow this app to access your mic please.... please.... please ", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+            
+        case .granted:
+            startRecording()
+        @unknown default:
+            break
         }
+    }
+    
+    func startRecording() {
+        do {
+            try prepareAudioSession()
+        } catch {
+            print("Error!")
+            return
+        }
+        
+        recordingURL = createNewRecordingURL()
+        
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: recordingURL!, format: format)
+            //audioRecorder.delegate = self
+            audioRecorder?.record()
+        } catch {
+            preconditionFailure("Audio recorder could not be created with url: \(recordingURL)")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+    }
+}
+
+extension AudioRecorderController: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if let recordingURL = recordingURL {
+            audioPlayer = try? AVAudioPlayer(contentsOf: recordingURL)
+        }
+        audioPlayer = nil
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let e = error {
+            print(e.localizedDescription)
+        }
+    }
+}
+
+extension AudioRecorderController: AVAudioPlayerDelegate {
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+        //cancelTimer()
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let e = error {
+            print("An error occured: \(e.localizedDescription)")
+        }
+    }
+}
+
+extension AudioRecorderController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        recordings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let recording = recordings[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AudioCell", for: indexPath) as! AudioCommentTableViewCell
+        
+        cell.nameLabel.text = recording.name
+        return cell
     }
 }
