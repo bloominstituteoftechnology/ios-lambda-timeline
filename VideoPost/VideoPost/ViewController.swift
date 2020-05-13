0a73a42da2476dd7e455045ebb6c6e782ec840a4
Jessie <jessie.griffin@icloud.com>
2020-05-12 21:02:57 -0400

    Creates VideoPost Project

---
+++ VideoPost/VideoPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPost
+//
+//  Created by Jessie Ann Griffin on 5/12/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
