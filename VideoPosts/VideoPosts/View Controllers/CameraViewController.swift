74b9e628bd85c804559fe1227212ecc9f5867aa1
David Wright <davidtwright89@gmail.com>
2020-05-12 23:32:21 -0400

    Created and hooked up the required IBOutlets and IBActions

--- VideoPosts/VideoPosts/View Controllers/CameraViewController.swift
+++ VideoPosts/VideoPosts/View Controllers/CameraViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class CameraViewController: UIViewController {
 
+    // IBOutlets
+    
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var cameraView: CameraPreviewView!
+    
+    // IBActions
+    
+    @IBAction func recordButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    // View Controller Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
