f3f6d7d318be794bbeb93c257a84d5a97df7c068
Jessie <jessie.griffin@icloud.com>
2020-05-14 23:25:39 -0400

    Adds Privacy descriptions for using location

---
+++ LambdaTimeline/Helpers/LocationManager.swift
@@ -0,0 +1,65 @@
+//
+//  GetLocation.swift
+//  LambdaTimeline
+//
+//  Created by Jessie Ann Griffin on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+class LocationManager: CLLocationManagerDelegate {
+    
+    init() {
+        
+    }
+    func isEqual(_ object: Any?) -> Bool {
+        <#code#>
+    }
+    
+    var hash: Int
+    
+    var superclass: AnyClass?
+    
+    func `self`() -> Self {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func isProxy() -> Bool {
+        <#code#>
+    }
+    
+    func isKind(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func isMember(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func conforms(to aProtocol: Protocol) -> Bool {
+        <#code#>
+    }
+    
+    func responds(to aSelector: Selector!) -> Bool {
+        <#code#>
+    }
+    
+    var description: String
+    
+    
+}
