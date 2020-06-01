82ce87a59643798627b3d420310ea1c3a0968cd6
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-01 13:32:40 -0700

    Added Navigation Controller to view controller

---
+++ PhotoFilterEditor/PhotoFilterEditor/ImagePostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImagePostViewController.swift
+//  PhotoFilterEditor
+//
+//  Created by Bhawnish Kumar on 6/1/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ImagePostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
