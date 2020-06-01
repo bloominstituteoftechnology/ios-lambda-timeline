b94b05cbeed6ff6cb74b06aa48445f6800859da8
Chris Dobek <dobek.christopher@gmail.com>
2020-06-01 17:32:10 -0400

    Outlets made

--- ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
+++ ImageFilterEditor/ImageFilterEditor/ImagePostViewController.swift
@@ -12,12 +12,28 @@ import CoreImage.CIFilterBuiltins
 import Photos
 
 class ImagePostViewController: UIViewController {
-
+    
+    // MARK: - Properites
+    private let context = CIContext(options: nil)
+    private var originalImage: UIImage?
+    private var scaledImage: UIImage?
+    
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var posterizeSlider: UISlider!
+    @IBOutlet weak var sepiaSlider: UISlider!
+    @IBOutlet weak var sharpnessSlider: UISlider!
+    @IBOutlet weak var radiusSlider: UISlider!
+    @IBOutlet weak var monoSlider: UISlider!
+    @IBOutlet weak var chromeSlider: UISlider!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        let filter = CIFilter.photoEffectMono()
-        print(filter)
+       
     }
 
 
