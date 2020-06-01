41fad88ee4c25104b94fee5dd95648a012d2c0ba
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-01 16:27:19 -0700

    Added IBOutlets for the sliders

--- PhotoFilterEditor/PhotoFilterEditor/ImagePostViewController.swift
+++ PhotoFilterEditor/PhotoFilterEditor/ImagePostViewController.swift
@@ -7,18 +7,62 @@
 //
 
 import UIKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import Photos
 
 class ImagePostViewController: UIViewController {
 
+    private var originalImage: UIImage?
     @IBOutlet weak var choosePhotoOutlet: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    @IBOutlet weak var blurRadiusSlider: UISlider!
+    @IBOutlet weak var bumpRadiusSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         choosePhotoOutlet.layer.cornerRadius = 12
         // Do any additional setup after loading the view.
+        let filter = CIFilter.colorControls()
+        filter.brightness = 1
+        print(filter.attributes)
+        
+        originalImage = imageView.image
     }
     
-
+    private func filterImage(_ image: UIImage) -> UIImage? {
+        
+        guard let cgImage = image.cgImage else { return nil }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        let filter = CIFilter.colorControls()
+        filter.brightness = 1
+        filter.saturation = 1
+        filter.contrast = 1
+        filter.inputImage = ciImage
+        
+        
+        return nil
+    }
+    
+    @IBAction func brightnessSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func contrastSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func saturationSlider(_ sender: UISlider) {
+    }
+    @IBAction func blurRadiusSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func bumpRadiusSlider(_ sender: UISlider) {
+    }
+    
     /*
     // MARK: - Navigation
 
