f29cd1578a86bd26edb84c1c83713e690dbb4f89
Chris Dobek <dobek.christopher@gmail.com>
2020-06-02 16:59:31 -0400

    Properites and Outlets made

--- AudioComments/AudioComments/AudioRecorderController.swift
+++ AudioComments/AudioComments/AudioRecorderController.swift
@@ -11,4 +11,22 @@ import AVFoundation
 
 class AudioRecorderController: UIViewController {
     
+    // MARK: Properties
+    private var timer: Timer?
+    private lazy var timeIntervalFormatter: DateComponentsFormatter = {
+        
+           let formatting = DateComponentsFormatter()
+           formatting.unitsStyle = .positional // 00:00  mm:ss
+           formatting.zeroFormattingBehavior = .pad
+           formatting.allowedUnits = [.minute, .second]
+           return formatting
+       }()
+    var recordingURL: URL?
+    var audioRecorder: AVAudioRecorder?
+    
+    // MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
 }
