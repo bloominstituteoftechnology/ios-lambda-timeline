8fff4a8c55a89e669b877664b9c6c3aa933271b2
Chris Dobek <dobek.christopher@gmail.com>
2020-06-02 17:05:35 -0400

    Added Recording Model

---
+++ AudioComments/AudioComments/RecordingModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecordingModel.swift
+//  AudioComments
+//
+//  Created by Chris Dobek on 6/2/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+
+struct Recording {
+    let name: String
+}
