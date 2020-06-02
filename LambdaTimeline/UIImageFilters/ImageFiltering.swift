bf85631bd9722061554a6efcf4c8c39123a90b5f
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-02 10:33:46 -0500

    Refactor storyboard to better fit the slider/filters
    Refactored the ImagePostVC to apply filters directly to chosen image

--- LambdaTimeline/UIImageFilters/ImageFiltering.swift
+++ LambdaTimeline/UIImageFilters/ImageFiltering.swift
@@ -20,42 +20,42 @@ class ImageFiltering: UIViewController {
     var delegate: PhotoFinishedEditing?
     
     private let context = CIContext(options: nil)
-        
+    
     var originalImage: UIImage? {
-            didSet {
-                
-                guard isViewLoaded else { return }
-                
-                guard let originalImage = originalImage else {
-                    scaledImage = nil // clear out image if original image fails
-                    return
-                }
-                
-                var scaledSize = imageView.bounds.size
-                let scale = UIScreen.main.scale
-                scaledSize = CGSize(width: scaledSize.width * scale,
-                                    height: scaledSize.height * scale)
-                scaledImage = originalImage.imageByScaling(toSize: scaledSize)
+        didSet {
+            
+            guard isViewLoaded else { return }
+            
+            guard let originalImage = originalImage else {
+                scaledImage = nil // clear out image if original image fails
+                return
             }
+            
+            var scaledSize = imageView.bounds.size
+            let scale = UIScreen.main.scale
+            scaledSize = CGSize(width: scaledSize.width * scale,
+                                height: scaledSize.height * scale)
+            scaledImage = originalImage.imageByScaling(toSize: scaledSize)
         }
-        
-        private var scaledImage: UIImage? {
-            didSet {
-                updateViews()
-            }
+    }
+    
+    private var scaledImage: UIImage? {
+        didSet {
+            updateViews()
         }
-        
-        @IBOutlet weak var brightnessSlider: UISlider!
-        @IBOutlet weak var contrastSlider: UISlider!
-        @IBOutlet weak var saturationSlider: UISlider!
-        @IBOutlet weak var blurSlider: UISlider!
-        @IBOutlet weak var imageView: UIImageView!
-        @IBOutlet weak var sharpnessSlider: UISlider!
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            originalImage = imageView.image
-        }
+    }
+    
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    @IBOutlet weak var blurSlider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var sharpnessSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        originalImage = imageView.image
+    }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -65,101 +65,101 @@ class ImageFiltering: UIViewController {
         blurSlider.value = 1
         sharpnessSlider.value = 0.4
     }
+    
+    private func filterImage(_ image: UIImage) -> UIImage? {
+        // UIImage -> CGImage -> CIImage
+        guard let cgImage = image.cgImage else { return nil }
         
-        private func filterImage(_ image: UIImage) -> UIImage? {
-            // UIImage -> CGImage -> CIImage
-            guard let cgImage = image.cgImage else { return nil }
-            
-            let ciImage = CIImage(cgImage: cgImage)
-            
-            // Filtering
-            let filter = CIFilter.colorControls() // May not work for some custom filters (KVC protocol)
-            filter.inputImage = ciImage
-            filter.brightness = brightnessSlider.value
-            filter.contrast = contrastSlider.value
-            filter.saturation = saturationSlider.value
-            
-            
-            // CIImage -> CGImage -> UIImage
-            guard let outputCIImage = filter.outputImage else { return nil }
-            
-            // Takes in the color filters and adds a blur filter
-            let blurFilter = CIFilter.gaussianBlur()
-            blurFilter.inputImage = outputCIImage
-            blurFilter.radius = blurSlider.value
-            
-            guard let blurFilterImage = blurFilter.outputImage else { return nil }
-            
-            // Applies a sharpen filter to the image
-            
-            let sharpenFilter = CIFilter.sharpenLuminance()
-            sharpenFilter.inputImage = blurFilterImage
-            sharpenFilter.sharpness = sharpnessSlider.value
-            
-            guard let sharpenFilterImage = sharpenFilter.outputImage else { return nil }
-            
-            //Render image
-            guard let outputCGImage = context.createCGImage(sharpenFilterImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
-            
-            return UIImage(cgImage: outputCGImage)
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        // Filtering
+        let filter = CIFilter.colorControls() // May not work for some custom filters (KVC protocol)
+        filter.inputImage = ciImage
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        filter.saturation = saturationSlider.value
+        
+        
+        // CIImage -> CGImage -> UIImage
+        guard let outputCIImage = filter.outputImage else { return nil }
+        
+        // Takes in the color filters and adds a blur filter
+        let blurFilter = CIFilter.gaussianBlur()
+        blurFilter.inputImage = outputCIImage
+        blurFilter.radius = blurSlider.value
+        
+        guard let blurFilterImage = blurFilter.outputImage else { return nil }
+        
+        // Applies a sharpen filter to the image
+        
+        let sharpenFilter = CIFilter.sharpenLuminance()
+        sharpenFilter.inputImage = blurFilterImage
+        sharpenFilter.sharpness = sharpnessSlider.value
+        
+        guard let sharpenFilterImage = sharpenFilter.outputImage else { return nil }
+        
+        //Render image
+        guard let outputCGImage = context.createCGImage(sharpenFilterImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    private func updateViews() {
+        guard let scaledImage = scaledImage else { return }
+        imageView.image = filterImage(scaledImage)
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func savePhotoButtonPressed(_ sender: UIBarButtonItem) {
+        // TODO: Save to photo library
+        saveAndFilterPhoto()
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func saveAndFilterPhoto() {
+        
+        //TODO: Use a delegate to pass the filtered photo to the postVC
+        guard let originalImage = originalImage else { return }
+        
+        guard let filteredImage = filterImage(originalImage) else { return }
+        
+        
+        PHPhotoLibrary.requestAuthorization { (status) in
+            guard status == .authorized else { return }
         }
         
-        private func updateViews() {
-            guard let scaledImage = scaledImage else { return }
-            imageView.image = filterImage(scaledImage)
-        }
-        
-        // MARK: Actions
-        
-        @IBAction func savePhotoButtonPressed(_ sender: UIBarButtonItem) {
-            // TODO: Save to photo library
-            saveAndFilterPhoto()
-            self.navigationController?.popViewController(animated: true)
-        }
-        
-        private func saveAndFilterPhoto() {
+        PHPhotoLibrary.shared().performChanges({
             
-            //TODO: Use a delegate to pass the filtered photo to the postVC
-            guard let originalImage = originalImage else { return }
+            PHAssetCreationRequest.creationRequestForAsset(from: filteredImage)
             
-            guard let filteredImage = filterImage(originalImage) else { return }
-            
-            
-            PHPhotoLibrary.requestAuthorization { (status) in
-                guard status == .authorized else { return }
+        }) { (success, error) in
+            if let error = error {
+                print("Error saving photo: 😢 \(error)")
+                return
             }
-            
-            PHPhotoLibrary.shared().performChanges({
-                
-                PHAssetCreationRequest.creationRequestForAsset(from: filteredImage)
-                
-            }) { (success, error) in
-                if let error = error {
-                    print("Error saving photo: 😢 \(error)")
-                    return
-                }
-                DispatchQueue.main.async {
-                    print("Saved photo")
-                }
+            DispatchQueue.main.async {
+                print("Saved photo")
             }
-            
-            delegate?.photoFinishedEditing(image: originalImage)
         }
         
-
-        // MARK: Slider events
-        
-        @IBAction func brightnessChanged(_ sender: UISlider) {
-            updateViews()
-        }
-        
-        @IBAction func contrastChanged(_ sender: Any) {
-            updateViews()
-        }
-        
-        @IBAction func saturationChanged(_ sender: Any) {
-            updateViews()
-        }
+        delegate?.photoFinishedEditing(image: originalImage)
+    }
+    
+    
+    // MARK: Slider events
+    
+    @IBAction func brightnessChanged(_ sender: UISlider) {
+        updateViews()
+    }
+    
+    @IBAction func contrastChanged(_ sender: Any) {
+        updateViews()
+    }
+    
+    @IBAction func saturationChanged(_ sender: Any) {
+        updateViews()
+    }
     
     @IBAction func blurChanged(_ sender: Any) {
         updateViews()
@@ -168,4 +168,4 @@ class ImageFiltering: UIViewController {
     @IBAction func sharpnessChanged(_ sender: Any) {
         updateViews()
     }
-    }
+}
--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -31,7 +31,9 @@ class ImagePostViewController: ShiftableViewController {
         
         setImageViewHeight(with: image.ratio)
         
-        imageView.image = image
+        originalImage = image
+        imageView.image = originalImage
+        
         
         chooseImageButton.setTitle("", for: [])
     }
@@ -53,13 +55,6 @@ class ImagePostViewController: ShiftableViewController {
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "EditPhotoShowSegue" {
-        let destinationVC = segue.destination as? ImageFiltering
-        destinationVC?.originalImage = imageView.image
-        
-        }
-    }
     
     @IBAction func createPost(_ sender: Any) {
         
@@ -123,16 +118,118 @@ class ImagePostViewController: ShiftableViewController {
         view.layoutSubviews()
     }
     
+    private func filterImage(_ image: UIImage) -> UIImage? {
+        // UIImage -> CGImage -> CIImage
+        guard let cgImage = image.cgImage else { return nil }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        // Filtering
+        let filter = CIFilter.colorControls() // May not work for some custom filters (KVC protocol)
+        filter.inputImage = ciImage
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        filter.saturation = saturationSlider.value
+        
+        
+        // CIImage -> CGImage -> UIImage
+        guard let outputCIImage = filter.outputImage else { return nil }
+        
+        // Takes in the color filters and adds a blur filter
+        let blurFilter = CIFilter.gaussianBlur()
+        blurFilter.inputImage = outputCIImage
+        blurFilter.radius = blurSlider.value
+        
+        guard let blurFilterImage = blurFilter.outputImage else { return nil }
+        
+        // Applies a sharpen filter to the image
+        
+        let sharpenFilter = CIFilter.sharpenLuminance()
+        sharpenFilter.inputImage = blurFilterImage
+        sharpenFilter.sharpness = sharpnessSlider.value
+        
+        guard let sharpenFilterImage = sharpenFilter.outputImage else { return nil }
+        
+        //Render image
+        guard let outputCGImage = context.createCGImage(sharpenFilterImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+        
+        print(filter.attributes, blurFilter.attributes, sharpenFilter.attributes)
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    private func updateScaleImage() {
+        guard let scaledImage = scaledImage else { return }
+        imageView.image = filterImage(scaledImage)
+    }
+    
     var postController: PostController!
     var post: Post?
     var imageData: Data?
+    private let context = CIContext(options: nil)
     
+    var originalImage: UIImage? {
+        didSet {
+            
+            guard let originalImage = originalImage else {
+                scaledImage = nil // clear out image if original image fails
+                return
+            }
+            
+            var scaledSize = imageView.bounds.size
+            let scale = UIScreen.main.scale
+            scaledSize = CGSize(width: scaledSize.width * scale,
+                                height: scaledSize.height * scale)
+            scaledImage = originalImage.imageByScaling(toSize: scaledSize)
+        }
+    }
+    
+    private var scaledImage: UIImage? {
+        didSet {
+            updateScaleImage()
+        }
+    }
+    
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    @IBOutlet weak var blurSlider: UISlider!
+    @IBOutlet weak var sharpnessSlider: UISlider!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var chooseImageButton: UIButton!
     @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var postButton: UIBarButtonItem!
+    
+    
+    // MARK: Slider events
+    
+    @IBAction func brightnessChanged(_ sender: UISlider) {
+        updateScaleImage()
+    }
+    
+    @IBAction func contrastChanged(_ sender: Any) {
+        updateScaleImage()
+        
+    }
+    
+    @IBAction func saturationChanged(_ sender: Any) {
+        updateScaleImage()
+    }
+    
+    @IBAction func blurChanged(_ sender: Any) {
+        updateScaleImage()
+    }
+    
+    @IBAction func sharpnessChanged(_ sender: Any) {
+        updateScaleImage()
+    }
+    
 }
+
+
+
+
 
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
