fa05462aa55445a7000a7722a455b5047a90389b
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-02 18:00:40 -0700

    Added tableview cell

---
+++ AudioComments/AudioComments/PlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerTableViewCell.swift
+//  AudioComments
+//
+//  Created by Bhawnish Kumar on 6/2/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
