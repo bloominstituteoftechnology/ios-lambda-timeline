858fc1248598560cb715c1cb4bd7d0b80d18037c
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-03 13:41:51 -0700

    Added selected image view to the button.

---
+++ VideoCollection/VideoCollection/CameraViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CameraViewController.swift
+//  VideoCollection
+//
+//  Created by Bhawnish Kumar on 6/3/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var cameraPreview: CameraPreviewView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
