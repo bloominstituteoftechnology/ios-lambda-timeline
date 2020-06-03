a50b32a9921ed5316bab01dfdc50afc1750b1887
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-03 14:12:22 -0700

    Fixed UIElement Constraints.

--- VideoCollection/VideoCollection/CameraViewController.swift
+++ VideoCollection/VideoCollection/CameraViewController.swift
@@ -9,27 +9,41 @@
 import UIKit
 
 class CameraViewController: UIViewController {
-
     
     
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var cameraView: CameraPreviewView!
     
-    @IBOutlet weak var cameraPreview: CameraPreviewView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setUpCaptureSession() {
+        
     }
-    */
-
+    
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+        
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
