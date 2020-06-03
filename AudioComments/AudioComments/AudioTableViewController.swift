160c374423118b9f5227c8f842ff949f7fdd79c0
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-02 18:17:07 -0700

    added segue identifier to connect the delegate.

--- AudioComments/AudioComments/AudioTableViewController.swift
+++ AudioComments/AudioComments/AudioTableViewController.swift
@@ -90,6 +90,11 @@ class AudioTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "detailSegue" {
+            guard let detailVC = segue.destination as? AudioPlayerViewController else { return }
+            detailVC.delegate = self 
+        }
     }
     
 
