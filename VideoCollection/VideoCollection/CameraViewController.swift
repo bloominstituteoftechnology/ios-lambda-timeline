606dfb320822732f434700e36cf3937a88292e93
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-03 15:05:23 -0700

    Camera recording successful & added Send UIElement

--- VideoCollection/VideoCollection/CameraViewController.swift
+++ VideoCollection/VideoCollection/CameraViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
-
+import AVFoundation
 class CameraViewController: UIViewController {
     
+    lazy var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    var player: AVPlayer?
+    var playerView: VideoPlayerView!
+
+    @IBOutlet weak var sendButton: UIButton!
     
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
@@ -17,17 +23,98 @@ class CameraViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUpCaptureSession()
+        cameraView.videoPlayerView.videoGravity = .resizeAspectFill
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // we will add the start and stop methods here because we don't wan't it to load only once.
+        captureSession.startRunning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
+    
+    
+    private func bestAudio() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(for: .audio) {
+            return device
+        }
+        fatalError("No audio")
+    }
+    
+    private func updateViews() {
+        self.recordButton.isSelected = fileOutput.isRecording
+    }
     
     func setUpCaptureSession() {
+        captureSession.beginConfiguration()
+        
+        let camera = bestCamera()
+        
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            showAlertMessage(title: "Camera Error", message: "Error in getting camera access", actiontitle: "Ok")
+            return
+        }
+        captureSession.addInput(cameraInput)
+        
+        let microphone = bestAudio()
+        guard let audioInput = try? AVCaptureDeviceInput(device: microphone),
+            captureSession.canAddInput(audioInput) else {
+                fatalError("Can't create and add input from microphone")
+        }
+        captureSession.addInput(audioInput)
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        guard captureSession.canAddOutput(fileOutput) else {
+            showAlertMessage(title: "Cannot add output", message: "Output not added", actiontitle: "Ok")
+            return
+        }
+        captureSession.addOutput(fileOutput)
+    
+        captureSession.commitConfiguration()
+        
+        cameraView.session = captureSession
         
     }
     
-    @IBAction func recordButtonPressed(_ sender: Any) {
+    private func bestCamera() -> AVCaptureDevice {
+        if let ultraWideCamera = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return ultraWideCamera
+        }
         
+        if let wideAngleCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) { // try .front
+            return wideAngleCamera
+        }
+        
+        // simulator or thr request hardware camera doesn't work
+        fatalError("Error in handling.")// TODO: show UI instead of a fatal error.
+        
+        
+    }
+    private func toggleRecording() {
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+            updateViews()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+    
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        toggleRecording()
+    }
+    
+    @IBAction func sendButtonTapped(_ sender: Any) {
     }
     
     
@@ -50,5 +137,60 @@ class CameraViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
+    func showAlertMessage(title: String, message: String, actiontitle: String) {
+        let endAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let endAction = UIAlertAction(title: actiontitle, style: .default) { (action: UIAlertAction ) in
+        }
+        
+        endAlert.addAction(endAction)
+        present(endAlert, animated: true, completion: nil)
+    }
+    
+    private func playMovie(url: URL) {
+        let player = AVPlayer(url: url)
+        
+        if playerView == nil {
+            
+            // setup view
+            
+            let playerView = VideoPlayerView()
+            playerView.player = player
+            
+            // customize frame
+            var frame = view.bounds
+            frame.size.height = frame.size.height / 4
+            frame.size.width = frame.size.width / 4
+            frame.origin.y = view.layoutMargins.top
+            playerView.frame = frame
+            view.addSubview(playerView)
+            self.playerView = playerView
+        }
+        player.play()
+        self.player = player
+    }
+}
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        print("Did start recording: \(fileURL)")
+        
+        updateViews()
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving movie: \(error)")
+            return
+        }
+        print("Play Movie!")
+        
+        DispatchQueue.main.async {
+            self.playMovie(url: outputFileURL)
+        }
+        
+        //play the movie if no error
+        updateViews()
+    }
     
 }
+
