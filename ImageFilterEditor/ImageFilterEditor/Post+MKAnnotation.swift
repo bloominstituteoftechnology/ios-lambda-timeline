a1add7f45bf4e0a4fa5b630c8e02811c2723d2a7
Chris Dobek <dobek.christopher@gmail.com>
2020-06-04 16:44:00 -0400

    Added Post+MKAnnotation file

---
+++ ImageFilterEditor/ImageFilterEditor/Post+MKAnnotation.swift
@@ -0,0 +1,15 @@
+//
+//  Post+MKAnnotation.swift
+//  ImageFilterEditor
+//
+//  Created by Chris Dobek on 6/4/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import MapKit
+
+extension Post: MKAnnotation {
+    
+}
+
+
