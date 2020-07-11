c3a1352eaa2e8f60ac9c8635058dc0b8924a3f96
Claudia Maciel <clc_80@yahoo.com>
2020-07-10 18:57:03 -0700

    Created View Controllers for TableView Controller

---
+++ ImageFilterEditor/ImageFilterEditor/AudioCommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AudioCommentTableViewCell.swift
+//  ImageFilterEditor
+//
+//  Created by Claudia Maciel on 7/10/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class AudioCommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
