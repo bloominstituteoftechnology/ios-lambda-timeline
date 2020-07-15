51a2358c76ff2b75dcc8807fcbb141c0c77d928a
Claudia Maciel <clc_80@yahoo.com>
2020-07-14 18:49:13 -0700

    Connected Code to the CreateVideo UI

---
+++ ImageFilterEditor/ImageFilterEditor/View Controller/CameraViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CameraViewController.swift
+//  ImageFilterEditor
+//
+//  Created by Claudia Maciel on 7/14/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    //MARK: - IBOutlets
+    @IBOutlet var cameraPreview: CameraPreviewView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
