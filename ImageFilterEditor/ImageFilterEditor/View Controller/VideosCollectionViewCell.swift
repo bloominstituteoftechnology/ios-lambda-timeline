524c154925115c36e2c75ce3c856d431ecb53c1f
Claudia Maciel <clc_80@yahoo.com>
2020-07-16 20:16:20 -0700

    Fixed Collection View to show the movie instead of image

--- ImageFilterEditor/ImageFilterEditor/View Controller/VideosCollectionViewCell.swift
+++ ImageFilterEditor/ImageFilterEditor/View Controller/VideosCollectionViewCell.swift
@@ -7,36 +7,83 @@
 //
 
 import UIKit
+import AVFoundation
 
 class VideosCollectionViewCell: UICollectionViewCell {
     
     // MARK: - IBOutlets
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var videoPlayerView: VideoPlayerView!
     
+    
+    // MARK: - Properties
     var videoClip: URL!
-    
+    var player: AVPlayer?
+    var isPlaying: Bool = false
     
     var post: Post? {
         didSet {
             updateViews()
         }
     }
-    var clipName: String? {
-        didSet {
-            titleLabel.text = clipName
-        }
-    }
     
-    var imageName: UIImage! {
-        didSet {
-            imageView.image = imageName
-        }
-    }
-    
+    // MARK: - Functions
     func updateViews() {
         guard let post = post else { return }
         titleLabel.text = post.title
-        imageView.image = post.image
+        
+        player = AVPlayer(url: post.mediaURL)
+        videoPlayerView.player = player
+        videoPlayerView?.videoPlayerLayer.videoGravity = .resizeAspectFill
+        player?.actionAtItemEnd = .pause
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        player?.pause()
+        player = nil
+        post = nil
+        isPlaying = false
+        imageView?.image = nil
+        titleLabel?.text = ""
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupMoviePlayerView()
+    }
+    
+       func setupMoviePlayerView() {
+           guard let moviePlayerView = videoPlayerView else { return }
+           let tapGesture = UITapGestureRecognizer(target: self.videoPlayerView, action: #selector(togglePlayMovie))
+           moviePlayerView.addGestureRecognizer(tapGesture)
+           
+           NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime,
+                                                  object: self.player?.currentItem,
+                                                  queue: .main) { [weak self] _ in
+                                                   self?.resetMovie()
+           }
+       }
+       
+       private func resetMovie() {
+           guard let player = player else { return }
+           
+           player.pause()
+           player.seek(to: .zero)
+           isPlaying = false
+       }
+       
+       @objc private func togglePlayMovie() {
+           guard let player = player else { return }
+           
+           if isPlaying {
+               player.pause()
+               isPlaying = false
+           } else {
+               player.play()
+               isPlaying = true
+           }
+       }
 }
