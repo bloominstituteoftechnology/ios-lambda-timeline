08a66c0ca1f498f1aaee51a2fd7d31862e87f605
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-02 19:20:05 -0700

    Created ImageFilterEditor Project. Added Filters and set up UI. Did not for repo leading to this being a larger commit.

---
+++ ImageFilterEditor/ImageFilterEditor/Filters.swift
@@ -0,0 +1,22 @@
+//
+//  Filters.swift
+//  ImageFilterEditor
+//
+//  Created by Clayton Watkins on 9/2/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import Foundation
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+
+class Filters {
+    
+    // MARK: - Properties
+    let blurFilter = CIFilter.gaussianBlur()
+    let falseColorFilter = CIFilter.falseColor()
+    let vignetteFilter = CIFilter.vignette()
+    let whitePointFilter = CIFilter.whitePointAdjust()
+    let monoChromaticFilter = CIFilter.photoEffectMono()
+}
