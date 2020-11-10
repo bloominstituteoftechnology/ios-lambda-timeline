fb0b61c935e1aa681f8da257a09b914619eb0bf4
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-09 19:35:59 -0700

    Added filters

--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class ImagePostViewController: ShiftableViewController {
@@ -30,10 +32,46 @@ class ImagePostViewController: ShiftableViewController {
     @IBOutlet weak var posterizeLabel: UILabel!
     @IBOutlet weak var posterizeSlider: UISlider!
     
+    private let context = CIContext()
+    private let toneFilter = CIFilter.sRGBToneCurveToLinear()
+    private let vintageFilter = CIFilter.photoEffectInstant()
+    private let noirFilter = CIFilter.photoEffectNoir()
+    private let exposureFilter = CIFilter.exposureAdjust()
+    private let vibranceFilter = CIFilter.vibrance()
+    private let posterizeFilter = CIFilter.colorPosterize()
     
     var postController: PostController!
     var post: Post?
     var imageData: Data?
+    var selectedFilter: CIFilter?
+    
+    var originalImage: UIImage? {
+        didSet {
+            guard let originalImage = originalImage else {
+                scaledImage = nil
+                return
+            }
+            
+            var scaledSize = imageView.bounds.size
+            let scale = imageView.contentScaleFactor
+            
+            scaledSize.width *= scale
+            scaledSize.height *= scale
+            
+            guard let scaledUIImage = originalImage.imageByScaling(toSize: scaledSize) else {
+                scaledImage = nil
+                return
+            }
+            
+            scaledImage = CIImage(image: scaledUIImage)
+        }
+    }
+    
+    var scaledImage: CIImage? {
+        didSet {
+            updateImage()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,6 +105,44 @@ class ImagePostViewController: ShiftableViewController {
             imagePicker.sourceType = .photoLibrary
             
             self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    private func image(byFiltering inputImage: CIImage, withFilter inputFilter: CIFilter?) -> UIImage? {
+        var output = inputImage
+        
+        if inputFilter == toneFilter {
+            toneFilter.inputImage = inputImage
+            output = toneFilter.outputImage!
+        } else if inputFilter == vintageFilter {
+            vintageFilter.inputImage = inputImage
+            output = vintageFilter.outputImage!
+        } else if inputFilter == noirFilter {
+            noirFilter.inputImage = inputImage
+            output = noirFilter.outputImage!
+        }
+        
+        exposureFilter.inputImage = output
+        exposureFilter.ev = exposureSlider.value
+        
+        vibranceFilter.inputImage = exposureFilter.outputImage
+        vibranceFilter.amount = vibranceSlider.value
+        
+        posterizeFilter.inputImage = vibranceFilter.outputImage
+        posterizeFilter.levels = posterizeSlider.value
+        
+        guard let outputImage = posterizeFilter.outputImage else { return nil }
+        
+        guard let renderedCGImage = context.createCGImage(outputImage, from: inputImage.extent) else { return nil }
+        
+        return UIImage(cgImage: renderedCGImage)
+    }
+    
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            imageView.image = image(byFiltering: scaledImage, withFilter: selectedFilter)
+        } else {
+            imageView.image = nil
         }
     }
     
@@ -127,31 +203,38 @@ class ImagePostViewController: ShiftableViewController {
     }
     
     @IBAction func clearFilters(_ sender: Any) {
-        
+        selectedFilter = nil
+        exposureSlider.value = 0.5
+        vibranceSlider.value = 0
+        posterizeSlider.value = 50
+        updateImage()
     }
     
     @IBAction func addToneFilter(_ sender: Any) {
-        
+        selectedFilter = toneFilter
+        updateImage()
     }
     
     @IBAction func addVintageFilter(_ sender: Any) {
-        
+        selectedFilter = vintageFilter
+        updateImage()
     }
     
     @IBAction func addNoirFilter(_ sender: Any) {
-        
+        selectedFilter = noirFilter
+        updateImage()
     }
     
     @IBAction func expoureChanged(_ sender: Any) {
-        
+        updateImage()
     }
     
     @IBAction func vibranceChanged(_ sender: Any) {
-        
+        updateImage()
     }
     
     @IBAction func posterizeChanged(_ sender: Any) {
-        
+        updateImage()
     }
 }
 
@@ -181,6 +264,12 @@ extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigation
         vibranceSlider.isHidden = false
         posterizeLabel.isHidden = false
         posterizeSlider.isHidden = false
+        
+        exposureSlider.value = 0.5
+        vibranceSlider.value = 0
+        posterizeSlider.value = 50
+        
+        originalImage = imageView.image
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
