036e1472909839aa50a0e6375ebc7d97e88eb586
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-10 02:40:34 -0700

    Added audio recording and playback functionality; completed project

--- LambdaTimeline/View Controllers/ImagePostDetailTableViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostDetailTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ImagePostDetailTableViewController: UITableViewController {
     
@@ -20,9 +21,29 @@ class ImagePostDetailTableViewController: UITableViewController {
     var postController: PostController!
     var imageData: Data?
     
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            guard let audioPlayer = audioPlayer else { return }
+            
+            audioPlayer.delegate = self
+            audioPlayer.isMeteringEnabled = true
+            updateViews()
+        }
+    }
+    
+    var isPlaying: Bool {
+        audioPlayer?.isPlaying ?? false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
     }
     
     func updateViews() {
@@ -41,6 +62,30 @@ class ImagePostDetailTableViewController: UITableViewController {
     
     @IBAction func createComment(_ sender: Any) {
         
+        let alert = UIAlertController(title: "New Comment", message: "Which kind of comment do you want to create?", preferredStyle: .actionSheet)
+        
+        let textCommentAction = UIAlertAction(title: "Text", style: .default) { (_) in
+            self.createTextComment()
+        }
+        
+        let audioCommentAction = UIAlertAction(title: "Audio", style: .default) { (_) in
+            self.createAudioComment()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(textCommentAction)
+        alert.addAction(audioCommentAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func createAudioComment() {
+        performSegue(withIdentifier: "AddAudioComment", sender: nil)
+    }
+    
+    func createTextComment() {
         let alert = UIAlertController(title: "Add a comment", message: "Write your comment below:", preferredStyle: .alert)
         
         var commentTextField: UITextField?
@@ -78,9 +123,73 @@ class ImagePostDetailTableViewController: UITableViewController {
         
         let comment = post?.comments[indexPath.row + 1]
         
-        cell.textLabel?.text = comment?.text
-        cell.detailTextLabel?.text = comment?.author
+        if comment?.audioURL == nil {
+            cell.textLabel?.text = comment?.text
+            cell.detailTextLabel?.text = comment?.author
+        } else if comment?.text == nil {
+            cell.textLabel?.text = "\(comment!.author)'s Audio Comment"
+            cell.detailTextLabel?.text = "Tap to play"
+        }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let comment = post?.comments[indexPath.row + 1]
+        
+        if comment?.audioURL == nil {
+            
+        } else if comment?.text == nil {
+            audioPlayer = try? AVAudioPlayer(contentsOf: (comment?.audioURL)!)
+            togglePlayback()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddAudioComment" {
+            if let audioVC = segue.destination as? AudioRecorderController {
+                audioVC.postController = postController
+                audioVC.post = post
+            }
+        }
+    }
+    
+    func togglePlayback() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func play() {
+        do {
+            try prepareAudioSession()
+            audioPlayer?.play()
+        } catch {
+            print("Cannot play audio: \(error)")
+        }
+    }
+    
+    func pause() {
+        audioPlayer?.pause()
+    }
+    
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+        try session.setActive(true, options: [])
+    }
+}
+
+extension ImagePostDetailTableViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("Audio Player Error: \(error)")
+        }
     }
 }
