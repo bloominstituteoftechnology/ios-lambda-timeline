5757306e38f5277e6a245fcd3c1e9fdfbf1cce5e
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-10 22:26:24 -0700

    Updated project to where I left off with audio comments; initial commit for video posts

--- LambdaTimeline/View Controllers/ImagePostDetailTableViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostDetailTableViewController.swift
@@ -7,31 +7,85 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ImagePostDetailTableViewController: UITableViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var imageViewAspectRatioConstraint: NSLayoutConstraint!
+    @IBOutlet weak var commentButton: UIBarButtonItem!
+    
+    var post: Post!
+    var postController: PostController!
+    var imageData: Data?
+    
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            guard let audioPlayer = audioPlayer else { return }
+            
+            audioPlayer.delegate = self
+            audioPlayer.isMeteringEnabled = true
+            updateViews()
+        }
+    }
+    
+    var isPlaying: Bool {
+        audioPlayer?.isPlaying ?? false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
     func updateViews() {
         
-        guard let imageData = imageData,
-            let image = UIImage(data: imageData) else { return }
+        guard case MediaType.image(let image) = post.mediaType else { return }
         
         title = post?.title
         
         imageView.image = image
         
         titleLabel.text = post.title
-        authorLabel.text = post.author.displayName
+        authorLabel.text = post.author
     }
     
     // MARK: - Table view data source
     
     @IBAction func createComment(_ sender: Any) {
         
+        let alert = UIAlertController(title: "New Comment", message: "Which kind of comment do you want to create?", preferredStyle: .actionSheet)
+        
+        let textCommentAction = UIAlertAction(title: "Text", style: .default) { (_) in
+            self.createTextComment()
+        }
+        
+        let audioCommentAction = UIAlertAction(title: "Audio", style: .default) { (_) in
+            self.createAudioComment()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(textCommentAction)
+        alert.addAction(audioCommentAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func createAudioComment() {
+        performSegue(withIdentifier: "AddAudioComment", sender: nil)
+    }
+    
+    func createTextComment() {
         let alert = UIAlertController(title: "Add a comment", message: "Write your comment below:", preferredStyle: .alert)
         
         var commentTextField: UITextField?
@@ -69,20 +123,73 @@ class ImagePostDetailTableViewController: UITableViewController {
         
         let comment = post?.comments[indexPath.row + 1]
         
-        cell.textLabel?.text = comment?.text
-        cell.detailTextLabel?.text = comment?.author.displayName
+        if comment?.audioURL == nil {
+            cell.textLabel?.text = comment?.text
+            cell.detailTextLabel?.text = comment?.author
+        } else if comment?.text == nil {
+            cell.textLabel?.text = "\(comment!.author)'s Audio Comment"
+            cell.detailTextLabel?.text = "Tap to play"
+        }
         
         return cell
     }
     
-    var post: Post!
-    var postController: PostController!
-    var imageData: Data?
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let comment = post?.comments[indexPath.row + 1]
+        
+        if comment?.audioURL == nil {
+            
+        } else if comment?.text == nil {
+            audioPlayer = try? AVAudioPlayer(contentsOf: (comment?.audioURL)!)
+            togglePlayback()
+        }
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddAudioComment" {
+            if let audioVC = segue.destination as? AudioRecorderController {
+                audioVC.postController = postController
+                audioVC.post = post
+            }
+        }
+    }
     
+    func togglePlayback() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var imageViewAspectRatioConstraint: NSLayoutConstraint!
+    func play() {
+        do {
+            try prepareAudioSession()
+            audioPlayer?.play()
+        } catch {
+            print("Cannot play audio: \(error)")
+        }
+    }
+    
+    func pause() {
+        audioPlayer?.pause()
+    }
+    
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+        try session.setActive(true, options: [])
+    }
+}
+
+extension ImagePostDetailTableViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("Audio Player Error: \(error)")
+        }
+    }
 }
